678dc089d0ec8e6e4c3e0951845deb378fa0b137
GitHub Action <action@github.com>
2024-05-30 11:56:13 +0000

    crates-io: update

--- crates-io/gi/t_/git_automation.scm
+++ crates-io/gi/t_/git_automation.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-git_automation-0.1.0 (c (n "git_automation") (v "0.1.0") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "names") (r "^0.14.0") (d #t) (k 0)))) (h "15vjw8jryx2cdr3k5dmaxc2llzjxjaq83nkh2ibzps2acqrfa8q3")))
 
+(define-public crate-git_automation-0.1.1 (c (n "git_automation") (v "0.1.1") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "names") (r "^0.14.0") (d #t) (k 0)))) (h "023ik8nzz5z2hr0zgdrp6hncasqrz4f33c06fnkcrlj30c7xwwk1")))
+
