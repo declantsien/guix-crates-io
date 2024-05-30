9efb8beaedcad94392f46b229847251886836462
GitHub Action <action@github.com>
2024-05-30 06:56:50 +0000

    crates-io: update

---
+++ crates-io/no/op/noop-allocator.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io no op noop-allocator) #:use-module (crates-io))
+
+(define-public crate-noop-allocator-0.1.0 (c (n "noop-allocator") (v "0.1.0") (h "1nkx94izy68z5pc61n3c18n3qbc37c4rafsb4x89ym4p45dy4i6i") (f (quote (("std" "alloc") ("default" "std") ("alloc"))))))
+
--- crates-io/sa/y-/say-hello-nicovicz.scm
+++ crates-io/sa/y-/say-hello-nicovicz.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-say-hello-nicovicz-0.2.0 (c (n "say-hello-nicovicz") (v "0.2.0") (h "08k0b9dxx0mvqkwdqa2jmahjcam582zd8ca4aj246yw45vflr3ha")))
 
+(define-public crate-say-hello-nicovicz-0.3.0 (c (n "say-hello-nicovicz") (v "0.3.0") (h "11qr5xbil6y6jxqk1mhfpa2a7ijvp1lax6mm42g538yyiha0q2si")))
+
