a51b8d597e1d1008173d8d3928042e4ccd8180d6
GitHub Action <action@github.com>
2024-05-30 20:56:03 +0000

    crates-io: update

---
+++ crates-io/ge/co/gecos.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ge co gecos) #:use-module (crates-io))
+
+(define-public crate-gecos-0.1.0 (c (n "gecos") (v "0.1.0") (d (list (d (n "thiserror") (r "^1.0.61") (d #t) (k 0)))) (h "0gzc4a4rq7c8x5477a7nm29s0ijdl11whl93qy47n4v2jk5w0lm9")))
+
