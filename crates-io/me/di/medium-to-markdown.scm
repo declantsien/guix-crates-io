2c1c1bd16b0b99f0cf639e5062b9c4d06a74ed1f
GitHub Action <action@github.com>
2024-05-30 06:34:32 +0000

    crates-io: update

--- crates-io/me/di/medium-to-markdown.scm
+++ crates-io/me/di/medium-to-markdown.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-medium-to-markdown-0.1.1 (c (n "medium-to-markdown") (v "0.1.1") (d (list (d (n "html_parser") (r "^0.7.0") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.3") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)))) (h "0v88nlgfg729fdysg9g622c626x7ihyk9y5ji6dm5s3x8ksfj02f")))
 
+(define-public crate-medium-to-markdown-0.1.2 (c (n "medium-to-markdown") (v "0.1.2") (d (list (d (n "html_parser") (r "^0.7.0") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.3") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("rt-multi-thread" "macros"))) (d #t) (k 0)))) (h "1q8j4jjf9qb2lhlpvi6x29hrrgngs4vz1j5a9w2v2zh4f9w7qal1")))
+
--- crates-io/sa/y-/say-hello-nicovicz.scm
+++ crates-io/sa/y-/say-hello-nicovicz.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-say-hello-nicovicz-0.1.0 (c (n "say-hello-nicovicz") (v "0.1.0") (h "0m5fzyzikhq5pzs81fm34v3izf0qg1safnk8vqvzpcdfvvncgawc")))
 
+(define-public crate-say-hello-nicovicz-0.2.0 (c (n "say-hello-nicovicz") (v "0.2.0") (h "08k0b9dxx0mvqkwdqa2jmahjcam582zd8ca4aj246yw45vflr3ha")))
+
