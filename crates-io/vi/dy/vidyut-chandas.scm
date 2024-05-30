e5f9e8a2385ef9536256587f392a783e9cd370c7
GitHub Action <action@github.com>
2024-05-30 05:31:31 +0000

    crates-io: update

---
+++ crates-io/vi/dy/vidyut-chandas.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io vi dy vidyut-chandas) #:use-module (crates-io))
+
+(define-public crate-vidyut-chandas-0.1.0 (c (n "vidyut-chandas") (v "0.1.0") (d (list (d (n "console_error_panic_hook") (r "^0.1.7") (d #t) (k 0)) (d (n "lazy_static") (r "^1.4.0") (d #t) (k 0)) (d (n "serde") (r "^1.0.150") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde-wasm-bindgen") (r "^0.4") (d #t) (k 0)) (d (n "serde_derive") (r "^1.0.193") (d #t) (k 0)) (d (n "wasm-bindgen") (r "^0.2") (d #t) (k 0)))) (h "0x041msdk1n70yns9d2i3gjgf6q8jah40xbakdmh7m758qmndnvk") (r "1.68")))
+
