7271b00d54f28ededa4ca31f64619b17a13408de
GitHub Action <action@github.com>
2024-05-30 14:48:59 +0000

    crates-io: update

--- crates-io/no/xp/noxp.scm
+++ crates-io/no/xp/noxp.scm
@@ -1,4 +1,8 @@
 (define-module (crates-io no xp noxp) #:use-module (crates-io))
 
-(define-public crate-noxp-0.1.0 (c (n "noxp") (v "0.1.0") (h "18ikgfrdgzc9g8qnz2j6fv2j9rawkyhg97y4ar9h4x4ks8s8b53k")))
+(define-public crate-noxp-0.1.0 (c (n "noxp") (v "0.1.0") (h "18ikgfrdgzc9g8qnz2j6fv2j9rawkyhg97y4ar9h4x4ks8s8b53k") (y #t)))
+
+(define-public crate-noxp-0.1.1 (c (n "noxp") (v "0.1.1") (h "165lgmpaj547js18fp2690ljixdgzcvbxbvv7z2ffcp75pbx390n") (y #t)))
+
+(define-public crate-noxp-0.1.2 (c (n "noxp") (v "0.1.2") (h "0li2969a83lki4qrrvsv541v11gg2z3y5h0rxb6xy83lbi9wl3rb")))
 
