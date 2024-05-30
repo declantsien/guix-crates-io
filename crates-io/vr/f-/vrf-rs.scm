d30ea195fd063be6ba0a38d792d28f8567e567d9
GitHub Action <action@github.com>
2024-05-30 15:38:32 +0000

    crates-io: update

---
+++ crates-io/vr/f-/vrf-rs.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io vr f- vrf-rs) #:use-module (crates-io))
+
+(define-public crate-vrf-rs-0.0.0 (c (n "vrf-rs") (v "0.0.0") (d (list (d (n "elliptic-curve") (r "^0.13") (f (quote ("sec1"))) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 2)) (d (n "hex-literal") (r "^0.4") (d #t) (k 2)) (d (n "k256") (r "^0.13") (f (quote ("expose-field" "hash2curve"))) (k 0)) (d (n "p256") (r "^0.13") (f (quote ("expose-field" "hash2curve"))) (k 0)) (d (n "rfc6979") (r "^0.4") (d #t) (k 0)) (d (n "serde_json") (r "^1.0.108") (d #t) (k 2)) (d (n "sha2") (r "^0.10") (d #t) (k 0)) (d (n "thiserror") (r "^1.0") (d #t) (k 0)))) (h "0vyh2a7qd7jg92iaxxk0r0gr1fi1q5sa7fcql3idsdsgkr6al158")))
+
