6bda4ac06b1642acae92a08e2162eeec31b95831
GitHub Action <action@github.com>
2024-05-30 11:46:55 +0000

    crates-io: update

--- crates-io/st/ag/staging-chain-spec-builder.scm
+++ crates-io/st/ag/staging-chain-spec-builder.scm
@@ -1,4 +1,6 @@
 (define-module (crates-io st ag staging-chain-spec-builder) #:use-module (crates-io))
 
-(define-public crate-staging-chain-spec-builder-0.0.0 (c (n "staging-chain-spec-builder") (v "0.0.0") (h "1i3s95cam7lmsba93q2hg2a46cknkjz1ifjvlmklhq6saq6ncqr2")))
+(define-public crate-staging-chain-spec-builder-0.0.0 (c (n "staging-chain-spec-builder") (v "0.0.0") (h "1i3s95cam7lmsba93q2hg2a46cknkjz1ifjvlmklhq6saq6ncqr2") (y #t)))
+
+(define-public crate-staging-chain-spec-builder-1.12.0 (c (n "staging-chain-spec-builder") (v "1.12.0") (d (list (d (n "clap") (r "^4.5.3") (f (quote ("derive"))) (d #t) (k 0)) (d (n "log") (r "^0.4.21") (d #t) (k 0)) (d (n "sc-chain-spec") (r "^32.0.0") (d #t) (k 0)) (d (n "serde_json") (r "^1.0.114") (d #t) (k 0)) (d (n "sp-tracing") (r "^17.0.0") (d #t) (k 0)))) (h "1pviignsm3j2pqj37fzsnccm21pvc7i286x1a0yq7zdia5d54hay")))
 
