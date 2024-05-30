c0161a7f6ef6f60bcfef9d36fa972d8c1202bbd5
GitHub Action <action@github.com>
2024-05-30 20:47:34 +0000

    crates-io: update

--- crates-io/ra/nt/rantz_random.scm
+++ crates-io/ra/nt/rantz_random.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-rantz_random-0.3.6 (c (n "rantz_random") (v "0.3.6") (d (list (d (n "bevy") (r "^0.13.2") (o #t) (k 0)) (d (n "fastrand") (r "^2.1.0") (d #t) (k 0)) (d (n "rantz_spatial2d") (r "^0.3.6") (o #t) (d #t) (k 0)))) (h "0f5b1c11ry6y57g1sl0272agpnphnd78yij3qqhzh40y95l598cd") (f (quote (("spatial2d" "rantz_spatial2d") ("default" "spatial2d" "bevy") ("bevy" "bevy/bevy_render"))))))
 
+(define-public crate-rantz_random-0.3.7 (c (n "rantz_random") (v "0.3.7") (d (list (d (n "bevy") (r "^0.13.2") (o #t) (k 0)) (d (n "fastrand") (r "^2.1.0") (d #t) (k 0)) (d (n "rantz_spatial2d") (r "^0.3.7") (o #t) (d #t) (k 0)))) (h "0crmsycay9lgk2vg521rlz5nwxahmh0xwldp1v8pfpvxbgf7djaj") (f (quote (("spatial2d" "rantz_spatial2d") ("default" "spatial2d" "bevy") ("bevy" "bevy/bevy_render"))))))
+
