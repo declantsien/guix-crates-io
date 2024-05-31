68eff8e1498422158602f4835e4b8bfb78e12453
GitHub Action <action@github.com>
2024-05-31 06:35:32 +0000

    crates-io: update

---
+++ crates-io/fl/uo/fluoride.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io fl uo fluoride) #:use-module (crates-io))
+
+(define-public crate-fluoride-0.0.0 (c (n "fluoride") (v "0.0.0") (h "0zg21x82irq7acw3shp26yla43dazaqnbf3awmv7qyw7qi9n4f6x")))
+
---
+++ crates-io/sp/in/spin-sqlite-connection.scm
@@ -0,0 +1,6 @@
+(define-module (crates-io sp in spin-sqlite-connection) #:use-module (crates-io))
+
+(define-public crate-spin-sqlite-connection-0.1.0 (c (n "spin-sqlite-connection") (v "0.1.0") (d (list (d (n "cfg_aliases") (r "^0.2") (d #t) (k 1)) (d (n "rusqlite") (r "^0.31") (d #t) (t "cfg(not(target_family = \"wasm\"))") (k 0)) (d (n "spin-sdk") (r "^3.0.1") (d #t) (k 0)))) (h "11rkgkzsdy8lbzp1p10bxdb1qjvn6y0k53wgkdnhg6whmj40jik0") (y #t)))
+
+(define-public crate-spin-sqlite-connection-0.1.1 (c (n "spin-sqlite-connection") (v "0.1.1") (d (list (d (n "cfg_aliases") (r "^0.2") (d #t) (k 1)) (d (n "rusqlite") (r "^0.31") (d #t) (t "cfg(not(target_family = \"wasm\"))") (k 0)) (d (n "spin-sdk") (r "^3.0.1") (d #t) (k 0)))) (h "1aw7dz2p61s7pjz27s1giq52b6rh63a75gnlqbxkbwp0arvgbhpb")))
+
