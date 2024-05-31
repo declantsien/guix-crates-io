649794b71a735fdab75fa2a4e294afbdb1966224
GitHub Action <action@github.com>
2024-05-31 21:46:53 +0000

    crates-io: update

---
+++ crates-io/ls/tn/lstnconn.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ls tn lstnconn) #:use-module (crates-io))
+
+(define-public crate-lstnconn-0.1.0 (c (n "lstnconn") (v "0.1.0") (d (list (d (n "hashbrown") (r "^0.14.5") (d #t) (k 0)) (d (n "idbag") (r "^0.1.2") (d #t) (k 0)) (d (n "parking_lot") (r "^0.12.3") (d #t) (k 0)) (d (n "protwrap") (r "^0.3.0") (f (quote ("tls" "tokio"))) (d #t) (k 0)) (d (n "tokio") (r "^1.38.0") (f (quote ("macros" "net" "rt"))) (d #t) (k 0)) (d (n "tokio") (r "^1.38.0") (f (quote ("io-util" "rt-multi-thread" "time"))) (d #t) (k 2)) (d (n "tokio-test") (r "^0.4.4") (d #t) (k 2)))) (h "1z9plldz9fbdfspg6q67m6h3z19bl7afwjfgqq1rxmf7hr580wk5") (r "1.56")))
+
