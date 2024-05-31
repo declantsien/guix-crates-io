3022fbd0171e1012ea90f7f99d416c2248370a94
GitHub Action <action@github.com>
2024-05-31 03:39:20 +0000

    crates-io: update

--- crates-io/me/tr/metre-macros.scm
+++ crates-io/me/tr/metre-macros.scm
@@ -8,3 +8,5 @@
 
 (define-public crate-metre-macros-0.2.1 (c (n "metre-macros") (v "0.2.1") (d (list (d (n "Inflector") (r "^0.11.4") (d #t) (k 0)) (d (n "darling") (r "^0.20.3") (d #t) (k 0)) (d (n "proc-macro2") (r "^1.0.66") (d #t) (k 0)) (d (n "quote") (r "^1.0.32") (d #t) (k 0)) (d (n "regex") (r "^1.10.4") (d #t) (k 0)) (d (n "regex_static") (r "^0.1.1") (d #t) (k 0)) (d (n "syn") (r "^2.0.27") (d #t) (k 0)))) (h "1kvj99sn1svq9mxlcj7hz2ishg2zr0a74yb4wnyf7dz4gnwwsp2s")))
 
+(define-public crate-metre-macros-0.2.2 (c (n "metre-macros") (v "0.2.2") (d (list (d (n "Inflector") (r "^0.11.4") (d #t) (k 0)) (d (n "darling") (r "^0.20.3") (d #t) (k 0)) (d (n "proc-macro2") (r "^1.0.66") (d #t) (k 0)) (d (n "quote") (r "^1.0.32") (d #t) (k 0)) (d (n "regex") (r "^1.10.4") (d #t) (k 0)) (d (n "regex_static") (r "^0.1.1") (d #t) (k 0)) (d (n "syn") (r "^2.0.27") (d #t) (k 0)))) (h "0vw6bn44rvpx9h8m4fynyx5487cwvg1kmk7nq3gs5wlsgqg9z4v0")))
+
