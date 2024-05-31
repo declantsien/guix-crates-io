3b598bc99e395645603b9af09e481479118d026f
GitHub Action <action@github.com>
2024-05-31 22:13:16 +0000

    crates-io: update

---
+++ crates-io/cy/wa/cyware-convert.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io cy wa cyware-convert) #:use-module (crates-io))
+
+(define-public crate-cyware-convert-1.0.6 (c (n "cyware-convert") (v "1.0.6") (d (list (d (n "bstr") (r "^1.1.0") (d #t) (k 0)) (d (n "data-encoding") (r "^2.3.3") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "md5") (r "^0.7.0") (d #t) (k 0)) (d (n "percent-encoding") (r "^2.2.0") (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "sha1") (r "^0.10.5") (d #t) (k 0)) (d (n "sha2") (r "^0.10.6") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.38") (d #t) (k 0)))) (h "1gcczc4z84v7cmwjdqaxcsgi4lx1byjfm9jl4dvqb9x53ppx04wd")))
+
--- crates-io/em/it/emitter-rs.scm
+++ crates-io/em/it/emitter-rs.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-emitter-rs-0.0.2 (c (n "emitter-rs") (v "0.0.2") (d (list (d (n "bincode") (r "^1.3.3") (d #t) (k 0)) (d (n "lazy_static") (r "^1.4.0") (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "08shnvnpwwmjc9c6qzmbnw9zgccp3fvh0pvhs8kbn8kvljdcmhgh")))
 
+(define-public crate-emitter-rs-0.0.3 (c (n "emitter-rs") (v "0.0.3") (d (list (d (n "bincode") (r "^1.3.3") (d #t) (k 0)) (d (n "lazy_static") (r "^1.4.0") (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)) (d (n "wasm-bindgen-futures") (r "^0.4.42") (d #t) (k 0)))) (h "16b92gmnl3zcjwijx8anrq1vshvpqfnfpsrcgdwv3cw119h25l18")))
+
