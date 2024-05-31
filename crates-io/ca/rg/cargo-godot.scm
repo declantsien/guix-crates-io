300b5c5bd5c19982918517954547a1d7ea087d15
GitHub Action <action@github.com>
2024-05-31 16:15:38 +0000

    crates-io: update

--- crates-io/ca/rg/cargo-godot.scm
+++ crates-io/ca/rg/cargo-godot.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-cargo-godot-0.1.0 (c (n "cargo-godot") (v "0.1.0") (d (list (d (n "cargo_metadata") (r "^0.18") (d #t) (k 0)) (d (n "clap") (r "^4.5") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0") (d #t) (k 0)) (d (n "thiserror") (r "^1.0") (d #t) (k 0)) (d (n "which") (r "^6.0") (d #t) (k 0)))) (h "1zlqq4w41icf35y46amz76mzrmb98zi9qx485gb7kvqcrrshpzh3")))
 
+(define-public crate-cargo-godot-0.2.0 (c (n "cargo-godot") (v "0.2.0") (d (list (d (n "cargo_metadata") (r "^0.18") (d #t) (k 0)) (d (n "clap") (r "^4.5") (f (quote ("derive"))) (d #t) (k 0)) (d (n "convert_case") (r "^0.6") (d #t) (k 0)) (d (n "pathdiff") (r "^0.2") (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0") (d #t) (k 0)) (d (n "thiserror") (r "^1.0") (d #t) (k 0)) (d (n "which") (r "^6.0") (d #t) (k 0)))) (h "0pj5bpbvwp62kd3w10ici610pzd9vrvsmj6n860pmw5lcr01r993")))
+
