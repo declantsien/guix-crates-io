f7097eae4c045f148a418574f1ad97032a210421
GitHub Action <action@github.com>
2024-05-31 22:39:55 +0000

    crates-io: update

---
+++ crates-io/fa/mc/famcache.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io fa mc famcache) #:use-module (crates-io))
+
+(define-public crate-famcache-1.1.2 (c (n "famcache") (v "1.1.2") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "12b7hyrs9c0ali3pdmdis0q5rxw7wpawknyan6hxf2ykpr9ximbs")))
+
