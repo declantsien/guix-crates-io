c150034c76d6be318b547090698c9713a279bc49
GitHub Action <action@github.com>
2024-05-31 23:13:04 +0000

    crates-io: update

--- crates-io/fa/mc/famcache.scm
+++ crates-io/fa/mc/famcache.scm
@@ -8,3 +8,9 @@
 
 (define-public crate-famcache-1.1.5 (c (n "famcache") (v "1.1.5") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "1csfnqrsmzbh7jaalglw8vk7y5pvs8sq06k489qgrqanj5bixl9r")))
 
+(define-public crate-famcache-1.1.6 (c (n "famcache") (v "1.1.6") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "0l5nk1w0awvgw11dkwb25nzg2d6q7402xx12ysna4xfkq9dqrn6s")))
+
+(define-public crate-famcache-1.1.7 (c (n "famcache") (v "1.1.7") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "18jwlr72b6r0mqgj92bfjf8mnafshixy7wqq3r0micyxxih6gi02")))
+
+(define-public crate-famcache-1.1.8 (c (n "famcache") (v "1.1.8") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "19fs5b7id26h32kayvi14yjd42538kmkfmrhdxpi1fy9z5zxrmk6")))
+
