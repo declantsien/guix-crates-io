c62cadb943071d039984bba1973a8c7f8cd4237e
GitHub Action <action@github.com>
2024-05-31 15:47:43 +0000

    crates-io: update

---
+++ crates-io/cl/ou/cloud.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io cl ou cloud) #:use-module (crates-io))
+
+(define-public crate-cloud-0.0.0 (c (n "cloud") (v "0.0.0") (h "1zjqlyks3ybf9mjhdzlcbpmrhn4m0ka5m967n9kh75nkp9smkd4b")))
+
--- crates-io/mu/si/musix.scm
+++ crates-io/mu/si/musix.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-musix-0.1.0 (c (n "musix") (v "0.1.0") (d (list (d (n "cc") (r "^1.0") (d #t) (k 1)) (d (n "cmake") (r "^0.1.50") (d #t) (k 1)))) (h "199kxbs7c53nwwbr72r08qc9zijvzny5wx6sdly8cyblw51nj45i")))
 
+(define-public crate-musix-0.2.0 (c (n "musix") (v "0.2.0") (d (list (d (n "cc") (r "^1.0") (d #t) (k 1)) (d (n "cmake") (r "^0.1.50") (d #t) (k 1)))) (h "0m0673lpdbz6gqsn7qm5zq458rs0jmv61675rpd80waa6q53kz62")))
+
