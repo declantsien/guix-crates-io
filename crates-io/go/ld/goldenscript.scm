f5153be10b6ea7375cc7367802139483cc50e4cd
GitHub Action <action@github.com>
2024-05-31 17:56:10 +0000

    crates-io: update

--- crates-io/go/ld/goldenscript.scm
+++ crates-io/go/ld/goldenscript.scm
@@ -8,3 +8,5 @@
 
 (define-public crate-goldenscript-0.4.0 (c (n "goldenscript") (v "0.4.0") (d (list (d (n "chrono") (r "^0.4.38") (d #t) (k 2)) (d (n "dateparser") (r "^0.2.1") (d #t) (k 2)) (d (n "goldenfile") (r "^1.5") (d #t) (k 0)) (d (n "nom") (r "^7.0") (d #t) (k 0)) (d (n "nom_locate") (r "^4.0") (d #t) (k 0)) (d (n "test_each_file") (r "^0.3.2") (d #t) (k 2)))) (h "1mk7snv73h3k19qnbn8cxqdhdj5irib2nhg92ycm0ia8ks384qx2")))
 
+(define-public crate-goldenscript-0.5.0 (c (n "goldenscript") (v "0.5.0") (d (list (d (n "goldenfile") (r "^1.5") (d #t) (k 0)) (d (n "nom") (r "^7.0") (d #t) (k 0)) (d (n "nom_locate") (r "^4.0") (d #t) (k 0)) (d (n "test_each_file") (r "^0.3.2") (d #t) (k 2)))) (h "1nhraaf34dnbcdfjgpflk38rzj72f4w4h8bcp8cis8nqnnb75jjm")))
+
