c95cf8369d53bd9c1cf61709b5117c6e57ac179e
GitHub Action <action@github.com>
2024-05-31 14:56:25 +0000

    crates-io: update

--- crates-io/fi/dg/fidget_math.scm
+++ crates-io/fi/dg/fidget_math.scm
@@ -2,7 +2,9 @@
 
 (define-public crate-fidget_math-0.1.0 (c (n "fidget_math") (v "0.1.0") (d (list (d (n "fidget") (r "^0.2.7") (d #t) (k 0)))) (h "1nyy4g87mbj6692965g7j0841d2k5h8hvqi9cidir5j6vqs7xba8")))
 
-(define-public crate-fidget_math-0.1.1 (c (n "fidget_math") (v "0.1.1") (d (list (d (n "fidget") (r "^0.2.7") (d #t) (k 0)))) (h "09ld16scrhlwyg8gnl4qfdv1paa5zy14w2mngjs7jw205d03sy0r")))
+(define-public crate-fidget_math-0.1.1 (c (n "fidget_math") (v "0.1.1") (d (list (d (n "fidget") (r "^0.2.7") (d #t) (k 0)))) (h "09ld16scrhlwyg8gnl4qfdv1paa5zy14w2mngjs7jw205d03sy0r") (y #t)))
 
-(define-public crate-fidget_math-0.3.0 (c (n "fidget_math") (v "0.3.0") (d (list (d (n "fidget") (r "^0.3") (d #t) (k 0)))) (h "10qpn5zvia2mpyvvnbg0ars2730acki1wkwc38xi3g2j53clay50")))
+(define-public crate-fidget_math-0.3.0 (c (n "fidget_math") (v "0.3.0") (d (list (d (n "fidget") (r "^0.3") (d #t) (k 0)))) (h "10qpn5zvia2mpyvvnbg0ars2730acki1wkwc38xi3g2j53clay50") (y #t)))
+
+(define-public crate-fidget_math-0.3.1 (c (n "fidget_math") (v "0.3.1") (d (list (d (n "fidget") (r "^0.3") (d #t) (k 0)))) (h "1bw4fhz8naw6flmci72359sbyd2hzin695gy8ykg9xbi3py3pqp2")))
 
