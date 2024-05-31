f46f557aae2414bb2ba2f73c23226f773c32a8ea
GitHub Action <action@github.com>
2024-05-31 19:39:03 +0000

    crates-io: update

---
+++ crates-io/cc/13/cc13x0_pac.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io cc #{13}# cc13x0_pac) #:use-module (crates-io))
+
+(define-public crate-cc13x0_pac-0.10.3 (c (n "cc13x0_pac") (v "0.10.3") (d (list (d (n "cortex-m") (r "^0.7.7") (d #t) (k 0)) (d (n "cortex-m-rt") (r "^0.7.3") (o #t) (d #t) (k 0)) (d (n "critical-section") (r "^1.0") (o #t) (d #t) (k 0)) (d (n "vcell") (r "^0.1.2") (d #t) (k 0)))) (h "0rwgkjcfcpwhbyd9i07ngz7h6zl5dfl77lbd3wg5krnkr0xzfik7") (f (quote (("rt" "cortex-m-rt/device"))))))
+
--- crates-io/wd/l-/wdl-core.scm
+++ crates-io/wd/l-/wdl-core.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-wdl-core-0.1.0 (c (n "wdl-core") (v "0.1.0") (d (list (d (n "clap") (r "^4.4.7") (f (quote ("derive"))) (d #t) (k 0)) (d (n "lazy_static") (r "^1.4.0") (d #t) (k 0)) (d (n "nonempty") (r "^0.9.0") (f (quote ("serialize"))) (d #t) (k 0)) (d (n "pest") (r "^2.7.5") (f (quote ("pretty-print"))) (d #t) (k 0)) (d (n "serde") (r "^1") (f (quote ("derive"))) (d #t) (k 0)) (d (n "to_snake_case") (r "^0.1.1") (d #t) (k 0)))) (h "19x0hs1alvfv0qnv6f0i5y6hc489vbn9prsap4n9ljc2r03d9nd3")))
 
+(define-public crate-wdl-core-0.2.0 (c (n "wdl-core") (v "0.2.0") (d (list (d (n "clap") (r "^4.4.7") (f (quote ("derive"))) (d #t) (k 0)) (d (n "convert_case") (r "^0.6.0") (d #t) (k 0)) (d (n "lazy_static") (r "^1.4.0") (d #t) (k 0)) (d (n "nonempty") (r "^0.9.0") (f (quote ("serialize"))) (d #t) (k 0)) (d (n "pest") (r "^2.7.5") (f (quote ("pretty-print"))) (d #t) (k 0)) (d (n "serde") (r "^1") (f (quote ("derive"))) (d #t) (k 0)))) (h "0s6il4z7wlfa11190si8f186m1zhwyb9nf87i31vc5hsnf7a24m7")))
+
