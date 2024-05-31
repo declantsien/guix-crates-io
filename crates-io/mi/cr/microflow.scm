337bfc0ef9eadddf48a3e321bc4b2f638fbfa0c8
GitHub Action <action@github.com>
2024-05-31 23:46:43 +0000

    crates-io: update

--- crates-io/mi/cr/microflow.scm
+++ crates-io/mi/cr/microflow.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-microflow-0.1.2 (c (n "microflow") (v "0.1.2") (d (list (d (n "criterion") (r "^0.5") (d #t) (k 2)) (d (n "csv") (r "^1.2") (d #t) (k 2)) (d (n "libm") (r "^0.2") (d #t) (k 0)) (d (n "microflow-macros") (r "^0.1") (d #t) (k 0)) (d (n "nalgebra") (r "^0.32") (f (quote ("macros"))) (k 0)) (d (n "simba") (r "^0.8") (k 0)))) (h "04aw32yn2d19zdaaqdfygd26k4291mchlqix56rnvv80k10lnm80")))
 
+(define-public crate-microflow-0.1.3 (c (n "microflow") (v "0.1.3") (d (list (d (n "criterion") (r "^0.5") (d #t) (k 2)) (d (n "csv") (r "^1.2") (d #t) (k 2)) (d (n "libm") (r "^0.2") (d #t) (k 0)) (d (n "microflow-macros") (r "^0.1") (d #t) (k 0)) (d (n "nalgebra") (r "^0.32") (f (quote ("macros"))) (k 0)) (d (n "simba") (r "^0.8") (k 0)))) (h "1vk3cxrgdykvk1lwpxgh83z4lwy3grincw5ivksrh5p67p4fl9xw")))
+
