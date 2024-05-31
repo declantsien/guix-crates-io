ee2417e7a33599addc49042530326d6eb00fe3e1
GitHub Action <action@github.com>
2024-05-31 13:57:29 +0000

    crates-io: update

---
+++ crates-io/es/p-/esp-hal-rmt-onewire.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io es p- esp-hal-rmt-onewire) #:use-module (crates-io))
+
+(define-public crate-esp-hal-rmt-onewire-0.1.0 (c (n "esp-hal-rmt-onewire") (v "0.1.0") (d (list (d (n "embassy-futures") (r "^0.1.1") (d #t) (k 0)) (d (n "esp-hal") (r "^0.17.0") (f (quote ("esp32c3" "embassy" "async"))) (d #t) (k 0)))) (h "0snq8fsxq55g5iwiwzr1j7j2rrr2s1qxw94aikf5bvf0k9099n54") (f (quote (("search-masks"))))))
+
---
+++ crates-io/ra/rc/rarc.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ra rc rarc) #:use-module (crates-io))
+
+(define-public crate-rarc-0.1.0 (c (n "rarc") (v "0.1.0") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "reqwest") (r "^0.12.4") (f (quote ("json" "blocking"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)))) (h "085acg0cs3wax6jmsz10v0ks8b0xy6qp5a223f2fc9f8wxklrz8y")))
+
