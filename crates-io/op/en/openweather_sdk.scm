1f5aaeea3fadf7c537586e8c1c8c53b91045d923
GitHub Action <action@github.com>
2024-05-31 06:18:07 +0000

    crates-io: update

--- crates-io/op/en/openweather_sdk.scm
+++ crates-io/op/en/openweather_sdk.scm
@@ -14,3 +14,5 @@
 
 (define-public crate-openweather_sdk-0.1.6 (c (n "openweather_sdk") (v "0.1.6") (d (list (d (n "dotenv") (r "^0.15.0") (d #t) (k 2)) (d (n "reqwest") (r "^0.11") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.152") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.107") (d #t) (k 2)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "124zxxqjsv0c9qbs4kj274agryb9m2z1040c5x5m8ippgakizfnv")))
 
+(define-public crate-openweather_sdk-0.1.7 (c (n "openweather_sdk") (v "0.1.7") (d (list (d (n "dotenv") (r "^0.15.0") (d #t) (k 2)) (d (n "reqwest") (r "^0.11") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.152") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.107") (d #t) (k 2)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "12g2fipryqca2i2mflyrbcc3fmykl0rb73q9bmh40pdpd6x8rg7i")))
+
---
+++ crates-io/so/ba/soba.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io so ba soba) #:use-module (crates-io))
+
+(define-public crate-soba-0.0.0 (c (n "soba") (v "0.0.0") (h "057x1b7gdbw54jmf5w6w0kbwhjkq7m3h8alr3rx4lblp2ansbv2k")))
+
