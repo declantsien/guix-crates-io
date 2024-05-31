2b1da340401113955a438f63959b2a40540bfa74
GitHub Action <action@github.com>
2024-05-31 18:16:52 +0000

    crates-io: update

--- crates-io/ch/in/chinese-rand.scm
+++ crates-io/ch/in/chinese-rand.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-chinese-rand-0.1.0 (c (n "chinese-rand") (v "0.1.0") (d (list (d (n "chinese-format") (r "^0.8.0") (d #t) (k 0)) (d (n "digit-sequence") (r "^0.3.4") (o #t) (d #t) (k 0)) (d (n "fastrand") (r "^2.1.0") (o #t) (d #t) (k 0)) (d (n "speculate2") (r "^0.2") (d #t) (k 2)))) (h "0c7j0dxd71078zfybzrfy0ii877nz8qabba3kpysmpzipchki8f7") (f (quote (("gregorian" "digit-sequence" "chinese-format/gregorian") ("default" "fastrand") ("currency" "chinese-format/currency")))) (s 2) (e (quote (("digit-sequence" "dep:digit-sequence" "chinese-format/digit-sequence"))))))
 
+(define-public crate-chinese-rand-0.1.1 (c (n "chinese-rand") (v "0.1.1") (d (list (d (n "chinese-format") (r "^0.8.0") (d #t) (k 0)) (d (n "digit-sequence") (r "^0.3.4") (o #t) (d #t) (k 0)) (d (n "fastrand") (r "^2.1.0") (o #t) (d #t) (k 0)) (d (n "speculate2") (r "^0.2") (d #t) (k 2)))) (h "0xbxjpnyf0512wp5v7zdgz9rpq79pw1p8xas7x5qj4f9nivkbmmr") (f (quote (("gregorian" "digit-sequence" "chinese-format/gregorian") ("default" "fastrand") ("currency" "chinese-format/currency")))) (s 2) (e (quote (("digit-sequence" "dep:digit-sequence" "chinese-format/digit-sequence"))))))
+
--- crates-io/mu/si/musix.scm
+++ crates-io/mu/si/musix.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-musix-0.2.0 (c (n "musix") (v "0.2.0") (d (list (d (n "cc") (r "^1.0") (d #t) (k 1)) (d (n "cmake") (r "^0.1.50") (d #t) (k 1)))) (h "0m0673lpdbz6gqsn7qm5zq458rs0jmv61675rpd80waa6q53kz62")))
 
+(define-public crate-musix-0.2.1 (c (n "musix") (v "0.2.1") (d (list (d (n "cc") (r "^1.0") (d #t) (k 1)) (d (n "cmake") (r "^0.1.50") (d #t) (k 1)))) (h "0rxp16klqrr8nvdwkbj453r7pz4wcr9alga8nq0kybksh5il0fic")))
+
