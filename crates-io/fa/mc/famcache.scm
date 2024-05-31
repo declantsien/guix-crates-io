60687aee969abb04c827cea53ff02661774b2092
GitHub Action <action@github.com>
2024-05-31 22:56:21 +0000

    crates-io: update

--- crates-io/fa/mc/famcache.scm
+++ crates-io/fa/mc/famcache.scm
@@ -4,3 +4,7 @@
 
 (define-public crate-famcache-1.1.3 (c (n "famcache") (v "1.1.3") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "1jfsam1c5mcffrkk84l89prd2smy39s3n03xclc0jg2m6616pwwl")))
 
+(define-public crate-famcache-1.1.4 (c (n "famcache") (v "1.1.4") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "1zxk8gdxk32pbgvzppmi1qq4r37cdsip0fkbhbw317rzjz2wxrid")))
+
+(define-public crate-famcache-1.1.5 (c (n "famcache") (v "1.1.5") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "1csfnqrsmzbh7jaalglw8vk7y5pvs8sq06k489qgrqanj5bixl9r")))
+
--- crates-io/li/gh/lighthouse-protocol.scm
+++ crates-io/li/gh/lighthouse-protocol.scm
@@ -24,3 +24,5 @@
 
 (define-public crate-lighthouse-protocol-3.3.0 (c (n "lighthouse-protocol") (v "3.3.0") (d (list (d (n "rand") (r "^0.8") (d #t) (k 0)) (d (n "rmpv") (r "^1.0.1") (f (quote ("with-serde"))) (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_with") (r "^3.4") (d #t) (k 0)))) (h "1h8jk1pkmv5z0gpl10sb3qmzjca9xczivfv57zz77k2b80sk3b8a")))
 
+(define-public crate-lighthouse-protocol-3.3.1 (c (n "lighthouse-protocol") (v "3.3.1") (d (list (d (n "rand") (r "^0.8") (d #t) (k 0)) (d (n "rmpv") (r "^1.0.1") (f (quote ("with-serde"))) (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_with") (r "^3.4") (d #t) (k 0)))) (h "1k2as9cwn8cajy9s1rrxb0kfbcd89sk259na133qm8mkdpz77ivw")))
+
