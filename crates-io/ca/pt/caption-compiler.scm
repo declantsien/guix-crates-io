d27011751ec390ef6747d010989f093a3e20a7d4
GitHub Action <action@github.com>
2024-06-01 07:56:08 +0000

    crates-io: update

--- crates-io/ca/pt/caption-compiler.scm
+++ crates-io/ca/pt/caption-compiler.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-caption-compiler-0.1.0 (c (n "caption-compiler") (v "0.1.0") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "crc32fast") (r "^1.4.2") (d #t) (k 0)) (d (n "encoding_rs") (r "^0.8.34") (d #t) (k 0)) (d (n "encoding_rs_io") (r "^0.1.7") (d #t) (k 0)) (d (n "keyvalues-parser") (r "^0.2.0") (d #t) (k 0)))) (h "0jjrlzr0yczipch5arbkc39qcx4qxc545qnj7iyv9mcdfq698vmv")))
 
+(define-public crate-caption-compiler-0.1.1 (c (n "caption-compiler") (v "0.1.1") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "crc32fast") (r "^1.4.2") (d #t) (k 0)) (d (n "encoding_rs") (r "^0.8.34") (d #t) (k 0)) (d (n "encoding_rs_io") (r "^0.1.7") (d #t) (k 0)) (d (n "keyvalues-parser") (r "^0.2.0") (d #t) (k 0)))) (h "17009y2yjgbd4gqns4gw1p7qgkdgnibnknj4awa0xyjci5dc71fj")))
+
