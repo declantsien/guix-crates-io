4ccdd1ca5baff1d3701330225daf424396b54909
GitHub Action <action@github.com>
2024-06-01 18:16:17 +0000

    crates-io: update

---
+++ crates-io/su/bi/subimpl.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io su bi subimpl) #:use-module (crates-io))
+
+(define-public crate-subimpl-0.1.0 (c (n "subimpl") (v "0.1.0") (d (list (d (n "syn") (r "^2.0.66") (d #t) (k 0)))) (h "0k9rlalqw46gphmg396ymrlvkgl4xg7aj1xjd3pzhyiaf4kdv03a")))
+
