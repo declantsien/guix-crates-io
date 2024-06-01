a4af1b3c17377c3d2f1b8db3f2fb716564ed87a1
GitHub Action <action@github.com>
2024-06-01 11:38:37 +0000

    crates-io: update

--- crates-io/lc/d-/lcd-lcm1602-i2c.scm
+++ crates-io/lc/d-/lcd-lcm1602-i2c.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-lcd-lcm1602-i2c-0.1.0 (c (n "lcd-lcm1602-i2c") (v "0.1.0") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)) (d (n "ufmt") (r "^0.1.0") (d #t) (k 0)) (d (n "ufmt-write") (r "^0.1.0") (d #t) (k 0)))) (h "158hcmamnw9759ydycxnf9mrkk14bkj442l8rvdbzy569xwky4kw")))
 
+(define-public crate-lcd-lcm1602-i2c-0.2.0 (c (n "lcd-lcm1602-i2c") (v "0.2.0") (d (list (d (n "embedded-hal") (r "^1.0.0") (d #t) (k 0)) (d (n "ufmt") (r "^0.2.0") (d #t) (k 0)) (d (n "ufmt-write") (r "^0.1.0") (d #t) (k 0)))) (h "1sdmavqzbw0mnyf5fzh5nj88vz413wfcnwdinvxwwmf9gvavqk0c")))
+
