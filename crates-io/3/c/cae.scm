cb449dabe078848401c907546605bd8d11b2f2a0
GitHub Action <action@github.com>
2024-06-01 17:56:32 +0000

    crates-io: update

---
+++ crates-io/3/c/cae.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io #{3}# c cae) #:use-module (crates-io))
+
+(define-public crate-cae-0.0.0 (c (n "cae") (v "0.0.0") (h "1nhhyxmwkbhq3cnx9fvrzwc3rg7v8l8ahrxfnn6b680nszbz0p3b")))
+
--- crates-io/li/bc/libcsp.scm
+++ crates-io/li/bc/libcsp.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-libcsp-0.1.1 (c (n "libcsp") (v "0.1.1") (d (list (d (n "bitflags") (r "^2") (d #t) (k 0)) (d (n "libc") (r "^0.2") (d #t) (k 0)) (d (n "libcsp-sys") (r "^0.1") (d #t) (k 0)) (d (n "num_enum") (r "^0.7") (d #t) (k 0)))) (h "1irankyw2q57a5mjcfcgvdkw7s7xvkd1y3ixld5k8i0q6zjk2jjp")))
 
+(define-public crate-libcsp-0.1.2 (c (n "libcsp") (v "0.1.2") (d (list (d (n "bitflags") (r "^2") (d #t) (k 0)) (d (n "libc") (r "^0.2") (d #t) (k 0)) (d (n "libcsp-sys") (r "^0.1") (d #t) (k 0)) (d (n "num_enum") (r "^0.7") (d #t) (k 0)))) (h "1cd79a060k6vbj96zwbixbpnb6q5ixja1riyii5fk8zmpqhdj2bl")))
+
