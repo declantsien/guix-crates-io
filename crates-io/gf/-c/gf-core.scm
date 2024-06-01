90f343bc5a8ca52bace74197ad9292c754608b1b
GitHub Action <action@github.com>
2024-06-01 04:41:12 +0000

    crates-io: update

---
+++ crates-io/gf/-c/gf-core.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io gf -c gf-core) #:use-module (crates-io))
+
+(define-public crate-gf-core-0.0.0-reserved (c (n "gf-core") (v "0.0.0-reserved") (h "0bg2pqcci137fi3ng63n17xdxyh3mnlw35dg600qajpagnfmrhv0")))
+
