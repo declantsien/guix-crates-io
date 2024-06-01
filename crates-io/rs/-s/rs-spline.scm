df68a596b999a65746ddb7555006ca97d9d3b49d
GitHub Action <action@github.com>
2024-06-01 19:45:56 +0000

    crates-io: update

---
+++ crates-io/rs/-s/rs-spline.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io rs -s rs-spline) #:use-module (crates-io))
+
+(define-public crate-rs-spline-0.0.0 (c (n "rs-spline") (v "0.0.0") (d (list (d (n "getrandom") (r "^0.2") (f (quote ("js"))) (d #t) (t "wasm32-unknown-unknown") (k 0)) (d (n "lazy_static") (r "^1") (d #t) (k 2)) (d (n "num") (r "^0.4") (k 0)) (d (n "serde") (r "^1") (f (quote ("derive"))) (o #t) (k 0)) (d (n "strum") (r "^0.26") (f (quote ("derive"))) (k 0)))) (h "14n99pks16mlvbss7443w81zzn8210agma25xhffyq0bbrykna4j") (f (quote (("wasm") ("wasi") ("rand" "num/rand") ("full" "default" "rand" "serde") ("default" "std")))) (s 2) (e (quote (("std" "alloc" "num/std" "serde?/std" "strum/std") ("serde" "dep:serde") ("alloc" "num/alloc" "serde?/alloc"))))))
+
