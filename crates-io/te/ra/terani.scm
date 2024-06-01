375566ac3ebd70578b6fd65d73a21482032f1822
GitHub Action <action@github.com>
2024-06-01 06:16:46 +0000

    crates-io: update

---
+++ crates-io/te/ra/terani.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io te ra terani) #:use-module (crates-io))
+
+(define-public crate-terani-0.1.0 (c (n "terani") (v "0.1.0") (d (list (d (n "colored") (r "^2.1.0") (d #t) (k 0)) (d (n "crossterm") (r "^0.27.0") (d #t) (k 0)) (d (n "rand") (r "^0.8.5") (d #t) (k 0)) (d (n "textwrap") (r "^0.16.1") (d #t) (k 0)))) (h "0axldr1lqa2cgynqx4p0i07sa1cpz8m8qhi3n4wqy0ipg7j735lz")))
+
