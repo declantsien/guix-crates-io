42ffa5705b3e611d0f5c9c030d4158d967a56945
GitHub Action <action@github.com>
2024-06-01 23:57:33 +0000

    crates-io: update

--- crates-io/as/yn/async-time-mock-core.scm
+++ crates-io/as/yn/async-time-mock-core.scm
@@ -8,3 +8,5 @@
 
 (define-public crate-async-time-mock-core-0.1.2 (c (n "async-time-mock-core") (v "0.1.2") (d (list (d (n "async-lock") (r "^3") (k 0)) (d (n "event-listener") (r "^4") (k 0)) (d (n "futures-lite") (r "^2") (k 2)) (d (n "pin-project-lite") (r "^0.2") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("rt" "macros"))) (d #t) (k 2)))) (h "1zxs0lsmn4nfc4zpzzrxxdzqkdrdcr2qh1nb2jsii487dhcdc0g7") (r "1.64")))
 
+(define-public crate-async-time-mock-core-0.1.3 (c (n "async-time-mock-core") (v "0.1.3") (d (list (d (n "async-lock") (r "^3") (k 0)) (d (n "event-listener") (r "^5") (k 0)) (d (n "futures-lite") (r "^2") (k 2)) (d (n "pin-project-lite") (r "^0.2") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("rt" "macros"))) (d #t) (k 2)))) (h "0vdjknywmpy23fiz7pvqy221axdb3y22qlj9ip0swfq56zbdwn4s") (r "1.64")))
+
