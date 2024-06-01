fd2a62c4c5d1b31ada07ac540818a738fcee1df7
GitHub Action <action@github.com>
2024-06-01 14:12:54 +0000

    crates-io: update

--- crates-io/rs/ch/rschess.scm
+++ crates-io/rs/ch/rschess.scm
@@ -2,5 +2,7 @@
 
 (define-public crate-rschess-0.1.0 (c (n "rschess") (v "0.1.0") (h "1w91v9lz99sb26ky7mgq44192l7x5ab9yldy4wjmbnx3rbsn6g36") (y #t)))
 
-(define-public crate-rschess-0.1.1 (c (n "rschess") (v "0.1.1") (h "0paj6p138yzjdwwllsa7q7693s4dy3gaqgl02hn00wmylkj1anrj")))
+(define-public crate-rschess-0.1.1 (c (n "rschess") (v "0.1.1") (h "0paj6p138yzjdwwllsa7q7693s4dy3gaqgl02hn00wmylkj1anrj") (y #t)))
+
+(define-public crate-rschess-0.2.0 (c (n "rschess") (v "0.2.0") (d (list (d (n "regex") (r "^1.10.4") (d #t) (k 0)))) (h "166qwfdd9gzcyf64h7bwf9xz69i0i4n3145fs5laak3hf1lc3qvl")))
 
---
+++ crates-io/sp/or/sportident-rs.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io sp or sportident-rs) #:use-module (crates-io))
+
+(define-public crate-sportident-rs-0.0.1 (c (n "sportident-rs") (v "0.0.1") (d (list (d (n "anyhow") (r "^1.0.81") (d #t) (k 0)) (d (n "bitflags") (r "^2") (d #t) (k 0)) (d (n "chrono") (r "^0.4.35") (d #t) (k 0)) (d (n "enum_dispatch") (r "^0.3.12") (d #t) (k 0)) (d (n "futures") (r "^0.3.30") (d #t) (k 0)) (d (n "strum") (r "^0.26") (d #t) (k 0)) (d (n "strum_macros") (r "^0.26") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.58") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("io-util" "macros" "rt-multi-thread"))) (d #t) (k 0)) (d (n "tokio-serial") (r "^5.0.5") (d #t) (k 0)) (d (n "tokio-util") (r "^0.7") (f (quote ("codec"))) (d #t) (k 0)) (d (n "tracing") (r "^0.1") (d #t) (k 0)) (d (n "tracing-subscriber") (r "^0.3") (d #t) (k 0)))) (h "0qlx5ai7fki3yj6ncq7sadap1h0yqvl15wlrlhl22fga4w90xch1")))
+
