2d664821ca00eb8872ee730dcebbe51954c5547a
GitHub Action <action@github.com>
2024-06-01 19:56:50 +0000

    crates-io: update

--- crates-io/lu/ne/lune-std-process.scm
+++ crates-io/lu/ne/lune-std-process.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-lune-std-process-0.1.0 (c (n "lune-std-process") (v "0.1.0") (d (list (d (n "directories") (r "^5.0") (d #t) (k 0)) (d (n "lune-utils") (r "^0.1.0") (d #t) (k 0)) (d (n "mlua") (r "^0.9.7") (f (quote ("luau"))) (d #t) (k 0)) (d (n "mlua-luau-scheduler") (r "^0.0.2") (d #t) (k 0)) (d (n "os_str_bytes") (r "^7.0") (f (quote ("conversions"))) (d #t) (k 0)) (d (n "pin-project") (r "^1.0") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("io-std" "io-util" "process" "rt" "sync"))) (k 0)))) (h "0l9pa3gyd0fly3sgjarhdi5pllmlhqgni39nyl43v2zfqvh92vk9")))
 
+(define-public crate-lune-std-process-0.1.1 (c (n "lune-std-process") (v "0.1.1") (d (list (d (n "directories") (r "^5.0") (d #t) (k 0)) (d (n "lune-utils") (r "^0.1.0") (d #t) (k 0)) (d (n "mlua") (r "^0.9.7") (f (quote ("luau"))) (d #t) (k 0)) (d (n "mlua-luau-scheduler") (r "^0.0.2") (d #t) (k 0)) (d (n "os_str_bytes") (r "^7.0") (f (quote ("conversions"))) (d #t) (k 0)) (d (n "pin-project") (r "^1.0") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("io-std" "io-util" "process" "rt" "sync"))) (k 0)))) (h "06l253vs001izwmfgbfs8k71giyn530w5j5yygn7lz5px8c070kj")))
+
--- crates-io/lu/ne/lune-std-roblox.scm
+++ crates-io/lu/ne/lune-std-roblox.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-lune-std-roblox-0.1.0 (c (n "lune-std-roblox") (v "0.1.0") (d (list (d (n "lune-roblox") (r "^0.1.0") (d #t) (k 0)) (d (n "lune-utils") (r "^0.1.0") (d #t) (k 0)) (d (n "mlua") (r "^0.9.7") (f (quote ("luau"))) (d #t) (k 0)) (d (n "mlua-luau-scheduler") (r "^0.0.2") (d #t) (k 0)) (d (n "once_cell") (r "^1.17") (d #t) (k 0)) (d (n "rbx_cookie") (r "^0.1.4") (k 0)))) (h "02qahwgw8qw286p1b9qp9s79x8prnmgakpzzl4hnnmnangv3a0s1")))
 
+(define-public crate-lune-std-roblox-0.1.1 (c (n "lune-std-roblox") (v "0.1.1") (d (list (d (n "lune-roblox") (r "^0.1.1") (d #t) (k 0)) (d (n "lune-utils") (r "^0.1.0") (d #t) (k 0)) (d (n "mlua") (r "^0.9.7") (f (quote ("luau"))) (d #t) (k 0)) (d (n "mlua-luau-scheduler") (r "^0.0.2") (d #t) (k 0)) (d (n "once_cell") (r "^1.17") (d #t) (k 0)) (d (n "rbx_cookie") (r "^0.1.4") (k 0)))) (h "08bks6gagk2zpghxfa1vcvn6cimgqhi2hilz8bpy0l7l46k97sb9")))
+
