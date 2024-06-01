c2481fc125af490363ccbbfddd5e44ab85c55608
GitHub Action <action@github.com>
2024-06-01 16:42:49 +0000

    crates-io: update

--- crates-io/bi/g_/big_num_math.scm
+++ crates-io/bi/g_/big_num_math.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-big_num_math-1.0.0 (c (n "big_num_math") (v "1.0.0") (h "0jk74ii6wghz3g99cdi8xd81slp31xmmxkk8nm894vs2m68mxwg3")))
 
+(define-public crate-big_num_math-1.0.1 (c (n "big_num_math") (v "1.0.1") (h "10qm564db31d7banqgmigp8b9k8ddm9r5jnbs5f0m0h9j7hwzjaq")))
+
---
+++ crates-io/tr/ac/trackpad_haptic.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io tr ac trackpad_haptic) #:use-module (crates-io))
+
+(define-public crate-trackpad_haptic-0.1.0 (c (n "trackpad_haptic") (v "0.1.0") (d (list (d (n "icrate") (r "^0.1") (f (quote ("AppKit_all" "Foundation_NSThread"))) (d #t) (k 0)))) (h "0b4l5jaa9n272n8zng0jysyk3bjs1wqlvr31hr4c3synl7lx9w27")))
+
