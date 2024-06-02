b775c8a6e6ddefece6361080c6bb7918d0cade92
GitHub Action <action@github.com>
2024-06-02 05:38:50 +0000

    crates-io: update

--- crates-io/ud/p-/udp-over-ssh.scm
+++ crates-io/ud/p-/udp-over-ssh.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-udp-over-ssh-0.1.0 (c (n "udp-over-ssh") (v "0.1.0") (d (list (d (n "async-std") (r "^1.12.0") (d #t) (k 0)) (d (n "base64") (r "^0.22.1") (d #t) (k 0)) (d (n "futures") (r "^0.3.30") (d #t) (k 0)))) (h "169qiavf2pjyxikryqsl7nz8agiia2dvb9mjpanq1av2r0wdspj9")))
 
+(define-public crate-udp-over-ssh-0.1.1 (c (n "udp-over-ssh") (v "0.1.1") (d (list (d (n "async-std") (r "^1.12.0") (d #t) (k 0)) (d (n "base64") (r "^0.22.1") (d #t) (k 0)) (d (n "futures") (r "^0.3.30") (d #t) (k 0)))) (h "120ag63j488vr5dbcnr9nyxwixv7awzz99z95nw5il1dl35fdasn")))
+
