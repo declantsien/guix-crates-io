486a28cf27faec78303bcb2396d253c8121240c1
GitHub Action <action@github.com>
2024-06-02 02:01:39 +0000

    crates-io: update

--- crates-io/re/ta/retained-macro.scm
+++ crates-io/re/ta/retained-macro.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-retained-macro-0.0.0 (c (n "retained-macro") (v "0.0.0") (d (list (d (n "proc-macro2") (r "^1.0.84") (d #t) (k 0)) (d (n "quote") (r "^1.0.36") (d #t) (k 0)) (d (n "syn") (r "^2.0") (f (quote ("full" "visit-mut" "visit"))) (d #t) (k 0)))) (h "087igz5wm3hxcsrnphiw40d247phc5rg1k29f3nzv9hmmlwzwrvs")))
 
+(define-public crate-retained-macro-0.0.1 (c (n "retained-macro") (v "0.0.1") (d (list (d (n "proc-macro2") (r "^1.0.84") (d #t) (k 0)) (d (n "quote") (r "^1.0.36") (d #t) (k 0)) (d (n "syn") (r "^2.0") (f (quote ("full" "visit-mut" "visit"))) (d #t) (k 0)))) (h "1x11awaifhl3c5p46fvw4cr1z6ykapmjvs30mkgpaw7j8h3igj47")))
+
--- crates-io/re/ta/retained.scm
+++ crates-io/re/ta/retained.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-retained-0.0.0 (c (n "retained") (v "0.0.0") (d (list (d (n "retained-macro") (r "^0.0.0") (d #t) (k 0)))) (h "1xizhvj7zdpcvasp9kf6h39gsxm3g6d3nrgl1yl1kgb53ywj0h0h")))
 
+(define-public crate-retained-0.0.1 (c (n "retained") (v "0.0.1") (d (list (d (n "retained-macro") (r "^0.0.1") (d #t) (k 0)))) (h "0m1fz5539vlzrw17ny9pafasc14syz1k4vkdadfsn45xhccmfmw1")))
+
