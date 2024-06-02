86d64fa061536ae0549ee5ae18c6dfb01256f847
GitHub Action <action@github.com>
2024-06-02 07:28:37 +0000

    crates-io: update

--- crates-io/or/d-/ord-reqwest.scm
+++ crates-io/or/d-/ord-reqwest.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-ord-reqwest-0.1.0 (c (n "ord-reqwest") (v "0.1.0") (d (list (d (n "ordinals") (r "^0.0.8") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.4") (d #t) (k 0)) (d (n "serde") (r "^1.0.198") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.116") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("rt" "rt-multi-thread" "macros"))) (d #t) (k 0)))) (h "1pwi6wwzs7qnyrn19ah5lwal85x53saxpm7ndj5p5imk71d4sr6y")))
 
+(define-public crate-ord-reqwest-0.1.1 (c (n "ord-reqwest") (v "0.1.1") (d (list (d (n "bitcoin") (r "^0.30.2") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "ordinals") (r "^0.0.8") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.4") (d #t) (k 0)) (d (n "serde") (r "^1.0.198") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.116") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("rt" "rt-multi-thread" "macros"))) (d #t) (k 0)))) (h "0imq4dzav0in9cvakclsbd4md728xql000ynhm1v0la9agbh3771")))
+
