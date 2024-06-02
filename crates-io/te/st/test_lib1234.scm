92c4bba20716587a335032a18c1a6fbf18030b4a
GitHub Action <action@github.com>
2024-06-02 03:49:22 +0000

    crates-io: update

---
+++ crates-io/te/st/test_lib1234.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io te st test_lib1234) #:use-module (crates-io))
+
+(define-public crate-test_lib1234-0.1.0 (c (n "test_lib1234") (v "0.1.0") (h "13ajjy6fhdj2wzjjpvbs43ga3b570s4y9i37f78svzf40lpzjc4k")))
+
