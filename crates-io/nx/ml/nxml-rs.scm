1f7ddd3f528cb458edc7da31186343996870785e
GitHub Action <action@github.com>
2024-06-02 04:15:26 +0000

    crates-io: update

---
+++ crates-io/nx/ml/nxml-rs.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io nx ml nxml-rs) #:use-module (crates-io))
+
+(define-public crate-nxml-rs-0.1.0 (c (n "nxml-rs") (v "0.1.0") (d (list (d (n "indexmap") (r "^2.2") (o #t) (d #t) (k 0)) (d (n "nxml-rs-macros") (r "^0.1.0") (d #t) (k 0)) (d (n "thiserror") (r "^1.0") (d #t) (k 0)))) (h "1nbp6digz57fd62xxpc2vdj7rzxcczrffz6apnn2prrqmzv8dmwc") (f (quote (("default" "indexmap")))) (s 2) (e (quote (("indexmap" "dep:indexmap"))))))
+
--- crates-io/pr/el/prelude-xml-parser.scm
+++ crates-io/pr/el/prelude-xml-parser.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-prelude-xml-parser-0.1.0 (c (n "prelude-xml-parser") (v "0.1.0") (d (list (d (n "chrono") (r "^0.4.38") (f (quote ("serde"))) (d #t) (k 0)) (d (n "pyo3") (r "^0.21.2") (o #t) (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde-xml-rs") (r "^0.6.0") (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.61") (d #t) (k 0)))) (h "13bs7v0fg2vn313cai20h4hsvfmzbfk1270gmfq9vbzblwgncagj") (s 2) (e (quote (("python" "dep:pyo3"))))))
 
+(define-public crate-prelude-xml-parser-0.2.0 (c (n "prelude-xml-parser") (v "0.2.0") (d (list (d (n "chrono") (r "^0.4.38") (f (quote ("serde"))) (d #t) (k 0)) (d (n "pyo3") (r "^0.21.2") (o #t) (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde-xml-rs") (r "^0.6.0") (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.61") (d #t) (k 0)))) (h "1y757ngkj1vs1nklz14f10xbgkdcyrg98aqbswvh7h0cgybpl4gv") (s 2) (e (quote (("python" "dep:pyo3"))))))
+
