b8704448222aed16a4c559c28f77b679512b2117
GitHub Action <action@github.com>
2024-06-02 21:56:08 +0000

    crates-io: update

---
+++ crates-io/bi/g_/big_lehmer.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io bi g_ big_lehmer) #:use-module (crates-io))
+
+(define-public crate-big_lehmer-0.1.0 (c (n "big_lehmer") (v "0.1.0") (d (list (d (n "dashu") (r "^0.4.2") (d #t) (k 0)) (d (n "rand") (r "^0.8.5") (f (quote ("small_rng"))) (d #t) (k 2)) (d (n "rayon") (r "^1.10.0") (d #t) (k 0)))) (h "1f45xvia7zq7mv8jj64mskx1ln936nb8kcd8vpxafk6q2z06bbg7")))
+
---
+++ crates-io/ra/ma/rama-macros.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ra ma rama-macros) #:use-module (crates-io))
+
+(define-public crate-rama-macros-0.2.0-alpha.0 (c (n "rama-macros") (v "0.2.0-alpha.0") (d (list (d (n "proc-macro2") (r "^1.0") (d #t) (k 0)) (d (n "quote") (r "^1.0") (d #t) (k 0)) (d (n "syn") (r "^2.0") (f (quote ("full" "parsing"))) (d #t) (k 0)) (d (n "syn") (r "^2.0") (f (quote ("full" "extra-traits"))) (d #t) (k 2)) (d (n "trybuild") (r "^1.0.63") (d #t) (k 2)))) (h "0x2q3sikjn3kmv1p32yp1ry8i6lar3x91k5w52kcqg7z5jazwlcl") (r "1.75.0")))
+
