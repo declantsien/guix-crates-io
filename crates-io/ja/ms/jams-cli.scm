190fa3640268417f0efafd25e187abfc1adb0b08
GitHub Action <action@github.com>
2024-06-02 15:50:09 +0000

    crates-io: update

--- crates-io/ja/ms/jams-cli.scm
+++ crates-io/ja/ms/jams-cli.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-jams-cli-0.1.0 (c (n "jams-cli") (v "0.1.0") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "clap") (r "^4.5") (f (quote ("derive"))) (d #t) (k 0)) (d (n "jams-core") (r "^0.1") (d #t) (k 0)))) (h "0zlwiv8da3wb0rssy3j0ba0p21cn7k0pdzgxi1rir11yhbg4jbjl")))
 
+(define-public crate-jams-cli-0.1.1 (c (n "jams-cli") (v "0.1.1") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "clap") (r "^4.5") (f (quote ("derive"))) (d #t) (k 0)) (d (n "jams-core") (r "^0.1") (d #t) (k 0)))) (h "0y1nbm5ybvwd43smldvx5yri5mwd7cai01dljnl29pqyxgjaw70h")))
+
