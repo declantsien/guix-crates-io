115fc8a2fa5510043f1ed11410c37c4a6a7b9202
GitHub Action <action@github.com>
2024-06-02 03:39:55 +0000

    crates-io: update

---
+++ crates-io/3/f/fni.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io #{3}# f fni) #:use-module (crates-io))
+
+(define-public crate-fni-0.0.1 (c (n "fni") (v "0.0.1") (h "1z3bq9n7amszhzkgzd6fwww89k4l55h1ld88qlvji04y0jr8qmgs")))
+
