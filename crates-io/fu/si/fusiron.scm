b244f9c4a93e0b377c82133dfe73a1b3de9c9234
GitHub Action <action@github.com>
2024-06-02 03:31:34 +0000

    crates-io: update

---
+++ crates-io/fu/si/fusiron.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io fu si fusiron) #:use-module (crates-io))
+
+(define-public crate-fusiron-0.0.1 (c (n "fusiron") (v "0.0.1") (h "1viwp530c0wzvv6dqp9jbqabrbi2885nrvyzh66irh1illvw55mb")))
+
---
+++ crates-io/yo/ce/yocer.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io yo ce yocer) #:use-module (crates-io))
+
+(define-public crate-yocer-0.0.1 (c (n "yocer") (v "0.0.1") (h "07y22gsfjg03s36206ivmmpxm9yypgi4zicgysrfiiq51pis9h51")))
+
