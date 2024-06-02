0dd9da049c07d1e21d97425a87dbf85d84d5eebb
GitHub Action <action@github.com>
2024-06-02 23:28:38 +0000

    crates-io: update

---
+++ crates-io/yo/ur/your-llm-chat.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io yo ur your-llm-chat) #:use-module (crates-io))
+
+(define-public crate-your-llm-chat-0.1.0 (c (n "your-llm-chat") (v "0.1.0") (d (list (d (n "bincode") (r "^1.3.3") (d #t) (k 0)) (d (n "chrono") (r "^0.4.38") (f (quote ("serde"))) (d #t) (k 0)) (d (n "env_logger") (r "^0.11.3") (d #t) (k 0)) (d (n "log") (r "^0.4.21") (d #t) (k 0)) (d (n "openai-api-rs") (r "^4.0.9") (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)))) (h "0iy8rw082dms1dr3hifdyvflfm3i98a2bl8js4490wpwdk86lkd6")))
+
