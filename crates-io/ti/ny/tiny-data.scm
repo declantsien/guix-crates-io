3bef8d7add6e416f29fd3dce95070f1ce53cc74a
GitHub Action <action@github.com>
2024-06-02 16:15:43 +0000

    crates-io: update

--- crates-io/ti/ny/tiny-data.scm
+++ crates-io/ti/ny/tiny-data.scm
@@ -2,3 +2,7 @@
 
 (define-public crate-tiny-data-0.1.0 (c (n "tiny-data") (v "0.1.0") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "futures") (r "^0.3") (d #t) (k 0)) (d (n "indicatif") (r "^0.17") (d #t) (k 0)) (d (n "regex") (r "^1.1") (d #t) (k 0)) (d (n "reqwest") (r "^0.12") (f (quote ("json" "stream"))) (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "0lsfc899fnsyafvd6zi6gx9radh3imq781b62ddws19fg3cggzz4")))
 
+(define-public crate-tiny-data-0.1.1 (c (n "tiny-data") (v "0.1.1") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "futures") (r "^0.3") (d #t) (k 0)) (d (n "indicatif") (r "^0.17") (d #t) (k 0)) (d (n "regex") (r "^1.1") (d #t) (k 0)) (d (n "reqwest") (r "^0.12") (f (quote ("json" "stream"))) (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "0c72w7cz2xm6zqvkdqlcs16nxj43qalbjwywh5s7nsl7xbhgx6ih")))
+
+(define-public crate-tiny-data-0.1.2 (c (n "tiny-data") (v "0.1.2") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("derive"))) (d #t) (k 0)) (d (n "futures") (r "^0.3") (d #t) (k 0)) (d (n "indicatif") (r "^0.17") (d #t) (k 0)) (d (n "regex") (r "^1.1") (d #t) (k 0)) (d (n "reqwest") (r "^0.12") (f (quote ("json" "stream"))) (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "0ng5spi8xv3qqhhrnsa9rfs0d8sma6y3kbksf85v2lk2l3myaj09")))
+
