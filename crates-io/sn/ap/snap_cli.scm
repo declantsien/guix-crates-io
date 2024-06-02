39ff4db9c8464d82c6a7d99c6a9918f49a7ea8eb
GitHub Action <action@github.com>
2024-06-02 23:57:17 +0000

    crates-io: update

--- crates-io/sn/ap/snap_cli.scm
+++ crates-io/sn/ap/snap_cli.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-snap_cli-0.1.0 (c (n "snap_cli") (v "0.1.0") (h "0wrycrwlvxbv06wk25wnj1x7l28cqc52f19w07drs0dqwvq1js2a")))
 
+(define-public crate-snap_cli-0.1.1 (c (n "snap_cli") (v "0.1.1") (h "0bl564cfrqdcba7gaqir7xl21k00hk9nvyk2zs6l444ymz79pz0g")))
+
