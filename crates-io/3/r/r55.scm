8e7994b06f14f12b4c83ee2ac186a2e5d9b05551
GitHub Action <action@github.com>
2024-06-02 19:46:22 +0000

    crates-io: update

---
+++ crates-io/3/r/r55.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io #{3}# r r55) #:use-module (crates-io))
+
+(define-public crate-r55-0.1.0 (c (n "r55") (v "0.1.0") (d (list (d (n "clap") (r "^4.5.4") (f (quote ("string"))) (d #t) (k 0)))) (h "1i00css2vyjiikflv8c074lzr8xg332lqwa6fi8lpfp73ix6ks6f")))
+
---
+++ crates-io/li/bl/liblitho.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io li bl liblitho) #:use-module (crates-io))
+
+(define-public crate-liblitho-0.1.0 (c (n "liblitho") (v "0.1.0") (d (list (d (n "clap") (r "^4.4.18") (f (quote ("derive" "cargo"))) (d #t) (k 0)) (d (n "env_logger") (r "^0.11.3") (d #t) (k 0)) (d (n "libc") (r "^0.2.152") (d #t) (k 0)) (d (n "mio") (r "^0.8.11") (d #t) (k 0)) (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0") (d #t) (k 0)) (d (n "sha2") (r "^0.10.8") (d #t) (k 0)) (d (n "simple-pub-sub") (r "^0.1.4") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full" "tracing"))) (d #t) (k 0)))) (h "0l8m4ni75q46d2w0jbdrqxpvfgyybc1nvljri5rra807dfw6m0c0")))
+
