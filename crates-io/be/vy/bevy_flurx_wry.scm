3512c099f23b8cd0a41459cf91a4af317c0a376f
GitHub Action <action@github.com>
2024-06-02 14:45:57 +0000

    crates-io: update

---
+++ crates-io/be/vy/bevy_flurx_wry.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io be vy bevy_flurx_wry) #:use-module (crates-io))
+
+(define-public crate-bevy_flurx_wry-0.1.0-alpha1 (c (n "bevy_flurx_wry") (v "0.1.0-alpha1") (d (list (d (n "bevy") (r "^0.13.2") (d #t) (k 2)) (d (n "bevy_flurx") (r "^0.5.2") (d #t) (k 2)) (d (n "bevy_flurx_ipc") (r "^0.1.0-alpha1") (d #t) (k 0)) (d (n "bevy_flurx_wry_api") (r "^0.1.0-alpha1") (d #t) (k 0)) (d (n "bevy_flurx_wry_core") (r "^0.1.0-alpha1") (d #t) (k 0)) (d (n "serde") (r "^1.0.202") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.202") (f (quote ("derive"))) (d #t) (k 2)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 2)))) (h "05wgvzrrwx17ab9j5ax4xz66zjglym7sigz0vqv4nij63nfd5l60")))
+
---
+++ crates-io/tu/ta/tutastinx.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io tu ta tutastinx) #:use-module (crates-io))
+
+(define-public crate-tutastinx-0.1.0 (c (n "tutastinx") (v "0.1.0") (d (list (d (n "rand") (r "^0.8.5") (d #t) (k 0)))) (h "1f0mp7i88qy89cd58j0fzmx2nwyan7gylxg9nb2ccl1a32dc7s51")))
+
