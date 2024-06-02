750c94a3e0d4fe712d008c0348f19a99a64035b9
GitHub Action <action@github.com>
2024-06-02 06:35:22 +0000

    crates-io: update

---
+++ crates-io/sc/op/scopefn.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io sc op scopefn) #:use-module (crates-io))
+
+(define-public crate-scopefn-0.0.1 (c (n "scopefn") (v "0.0.1") (d (list (d (n "either") (r "^1.12.0") (o #t) (d #t) (k 0)))) (h "1671d2d3xmvmq8fih682q922s8yzz3mqiyhfjsfxvv5lghflpx94") (f (quote (("run_if" "either"))))))
+
