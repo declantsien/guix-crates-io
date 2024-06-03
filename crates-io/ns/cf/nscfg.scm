c0485ca984f26275b9961e38a6196ece9a2abf2d
GitHub Action <action@github.com>
2024-06-03 19:31:24 +0000

    crates-io: update

---
+++ crates-io/ns/cf/nscfg.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ns cf nscfg) #:use-module (crates-io))
+
+(define-public crate-nscfg-1.0.0 (c (n "nscfg") (v "1.0.0") (h "1hllfkx38pd3ys466l2yj5fxwvj87mxi23a8hpcpcwc4y0ps8607")))
+
