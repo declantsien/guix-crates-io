ad451d110c0912ff65846fcd86e1b8bae832db11
GitHub Action <action@github.com>
2024-06-03 19:01:04 +0000

    crates-io: update

--- crates-io/ru/st/rust_jsc_macros.scm
+++ crates-io/ru/st/rust_jsc_macros.scm
@@ -8,3 +8,5 @@
 
 (define-public crate-rust_jsc_macros-0.1.4 (c (n "rust_jsc_macros") (v "0.1.4") (d (list (d (n "quote") (r "^1.0.36") (d #t) (k 0)) (d (n "syn") (r "^2.0.63") (f (quote ("full"))) (d #t) (k 0)))) (h "0ab8q2z4lwv60fab5jklqswllrfp3w0vadx9bvcyd49cj2ymqg1y")))
 
+(define-public crate-rust_jsc_macros-0.1.6 (c (n "rust_jsc_macros") (v "0.1.6") (d (list (d (n "quote") (r "^1.0.36") (d #t) (k 0)) (d (n "syn") (r "^2.0.63") (f (quote ("full"))) (d #t) (k 0)))) (h "0ccrnq2pz078h3hhrqi12nx0033bh173l02r7n9dgpbzj0lwi093")))
+
