78a27a0d90605562688464e7b9ab18a2a8b9bb7c
GitHub Action <action@github.com>
2024-06-03 20:40:33 +0000

    crates-io: update

---
+++ crates-io/bu/ll/bullet_stream.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io bu ll bullet_stream) #:use-module (crates-io))
+
+(define-public crate-bullet_stream-0.1.0 (c (n "bullet_stream") (v "0.1.0") (d (list (d (n "ascii_table") (r "^4.0.3") (f (quote ("color_codes"))) (d #t) (k 2)) (d (n "fun_run") (r "^0.1.2") (d #t) (k 2)) (d (n "indoc") (r "^2.0.5") (d #t) (k 2)) (d (n "libcnb-test") (r "^0.21.0") (d #t) (k 2)) (d (n "tempfile") (r "^3.10.1") (d #t) (k 2)))) (h "0qsyv24d69w37gbjvll1ivrpixy0wgdiccnarvmvba8n13dx6m1w")))
+
---
+++ crates-io/sc/x_/scx_rlfifo.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io sc x_ scx_rlfifo) #:use-module (crates-io))
+
+(define-public crate-scx_rlfifo-0.0.3 (c (n "scx_rlfifo") (v "0.0.3") (d (list (d (n "anyhow") (r "^1.0.65") (d #t) (k 0)) (d (n "ctrlc") (r "^3.1") (f (quote ("termination"))) (d #t) (k 0)) (d (n "libbpf-rs") (r "^0.23.1") (d #t) (k 0)) (d (n "libc") (r "^0.2.137") (d #t) (k 0)) (d (n "plain") (r "^0.2.3") (d #t) (k 0)) (d (n "scx_rustland_core") (r "^0.5") (d #t) (k 0)) (d (n "scx_rustland_core") (r "^0.5") (d #t) (k 1)) (d (n "scx_utils") (r "^0.8.1") (d #t) (k 0)) (d (n "scx_utils") (r "^0.8.1") (d #t) (k 1)))) (h "00hc0q82mfaif2l8hbr1wlyxav7qb83w56f2syyhjfjgrys9sdwk") (f (quote (("enable_backtrace"))))))
+
