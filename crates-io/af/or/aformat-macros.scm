8218b3f5aa944778ce3439052ded272f688de962
GitHub Action <action@github.com>
2024-06-03 23:56:01 +0000

    crates-io: update

---
+++ crates-io/af/or/aformat-macros.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io af or aformat-macros) #:use-module (crates-io))
+
+(define-public crate-aformat-macros-0.1.0 (c (n "aformat-macros") (v "0.1.0") (d (list (d (n "bytestring") (r "^1.3.1") (d #t) (k 0)) (d (n "proc-macro2") (r "^1.0.82") (d #t) (k 0)) (d (n "procout") (r "^0.1.13") (f (quote ("procout"))) (d #t) (k 0)) (d (n "quote") (r "^1.0.36") (d #t) (k 0)) (d (n "syn") (r "^2.0.61") (f (quote ("parsing" "proc-macro"))) (k 0)))) (h "1yk39llr25n7gb5ywwp4iwp43iqimf627wgz331wvrzziikrw4i1")))
+
---
+++ crates-io/af/or/aformat.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io af or aformat) #:use-module (crates-io))
+
+(define-public crate-aformat-0.1.0 (c (n "aformat") (v "0.1.0") (d (list (d (n "aformat-macros") (r "^0.1.0") (d #t) (k 0)) (d (n "arrayvec") (r "^0.7.4") (d #t) (k 0)) (d (n "to-arraystring") (r "^0.2.0") (d #t) (k 0)))) (h "0379ic2gx16kvi1bxm4ci3lm7vz5kajgk3n4ib0rvbk8s8m1i0bw")))
+
