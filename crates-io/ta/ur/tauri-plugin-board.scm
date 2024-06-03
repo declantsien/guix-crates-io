3e3fd41e3d9ae6bc2eb76d5a5862a563f23f553a
GitHub Action <action@github.com>
2024-06-03 09:56:32 +0000

    crates-io: update

---
+++ crates-io/ta/ur/tauri-plugin-board.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ta ur tauri-plugin-board) #:use-module (crates-io))
+
+(define-public crate-tauri-plugin-board-1.0.0 (c (n "tauri-plugin-board") (v "1.0.0") (d (list (d (n "tauri") (r "^2.0.0-beta") (d #t) (k 0)) (d (n "tauri-build") (r "^2.0.0-beta.17") (d #t) (k 1)) (d (n "tauri-plugin") (r "^2.0.0-beta.17") (d #t) (k 1)) (d (n "tauri-plugin-cli") (r "^2.0.0-beta") (d #t) (k 0)))) (h "10c2ampy83fxvsflkawa17h27x9y1h9fmlh8dsp5abx9brbpjcby") (l "") (r "1.60")))
+
