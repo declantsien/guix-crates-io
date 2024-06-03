89950c47a4e64f3f6181198760b96d3f3c0befbc
GitHub Action <action@github.com>
2024-06-03 02:58:14 +0000

    crates-io: update

--- crates-io/wm/lu/wmlua.scm
+++ crates-io/wm/lu/wmlua.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-wmlua-0.1.0 (c (n "wmlua") (v "0.1.0") (h "0d6gk0j6760bx1i5z7w5qvnc0843xs4wghp7m0511qw17ckgvhxp")))
 
+(define-public crate-wmlua-0.1.1 (c (n "wmlua") (v "0.1.1") (d (list (d (n "ffi") (r "^0.1.0") (d #t) (k 0) (p "wmlua-src")) (d (n "libc") (r "^0.2.1") (d #t) (k 0)))) (h "1jrqbwv35l8lhdkg1knhpv7byrc0g49h61l3gi1a659a0ngyma30") (f (quote (("luajit52" "luajit" "ffi/luajit52") ("luajit" "ffi/luajit") ("lua54" "ffi/lua54") ("lua53" "ffi/lua53") ("lua52" "ffi/lua52") ("lua51" "ffi/lua51"))))))
+
