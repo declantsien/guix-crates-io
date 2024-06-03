aa9abf210f2b1a2254ef6c2643136fe22a388aeb
GitHub Action <action@github.com>
2024-06-03 11:23:45 +0000

    crates-io: update

--- crates-io/sc/op/scopegraphs-render-docs.scm
+++ crates-io/sc/op/scopegraphs-render-docs.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-scopegraphs-render-docs-0.2.11 (c (n "scopegraphs-render-docs") (v "0.2.11") (d (list (d (n "include_dir") (r "^0.7") (d #t) (k 0)) (d (n "itertools") (r "^0.10") (d #t) (k 0)) (d (n "pretty_assertions") (r "^1") (d #t) (k 2)) (d (n "proc-macro-error") (r "^1") (k 0)) (d (n "proc-macro2") (r "^1") (d #t) (k 0)) (d (n "quote") (r "^1") (d #t) (k 0)) (d (n "syn") (r "^2") (d #t) (k 0)))) (h "1mdwnf94f4in5phbxklwf09dcihpf016b6mnxmnb49g01y2mzfy7")))
 
+(define-public crate-scopegraphs-render-docs-0.2.12 (c (n "scopegraphs-render-docs") (v "0.2.12") (d (list (d (n "include_dir") (r "^0.7") (d #t) (k 0)) (d (n "itertools") (r "^0.10") (d #t) (k 0)) (d (n "pretty_assertions") (r "^1") (d #t) (k 2)) (d (n "proc-macro-error") (r "^1") (k 0)) (d (n "proc-macro2") (r "^1") (d #t) (k 0)) (d (n "quote") (r "^1") (d #t) (k 0)) (d (n "syn") (r "^2") (d #t) (k 0)))) (h "13pca4kfl21vlj8im3bi3l17f4cxf71a0g1qw0s3hp4v5ywxwwl2")))
+
