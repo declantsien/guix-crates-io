7f827e56b0d040c8c3515cb5e4c133302c5e8ee7
GitHub Action <action@github.com>
2024-06-04 10:55:51 +0000

    crates-io: update

---
+++ crates-io/kr/os/krossbar-settings-common.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io kr os krossbar-settings-common) #:use-module (crates-io))
+
+(define-public crate-krossbar-settings-common-0.5.0 (c (n "krossbar-settings-common") (v "0.5.0") (d (list (d (n "serde") (r "^1.0") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0") (d #t) (k 0)) (d (n "tempdir") (r "^0.3.7") (d #t) (k 2)) (d (n "thiserror") (r "^1.0") (d #t) (k 0)))) (h "1kx4j9qrvhfjd8camplshs85k3qbs1xrmd4gnn6ir1n5clmrss8r") (r "1.75")))
+
--- crates-io/ut/f8/utf8-tokio.scm
+++ crates-io/ut/f8/utf8-tokio.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-utf8-tokio-0.1.0 (c (n "utf8-tokio") (v "0.1.0") (d (list (d (n "tokio") (r "^1") (f (quote ("io-util"))) (k 0)) (d (n "tokio") (r "^1") (f (quote ("macros" "rt-multi-thread"))) (k 2)) (d (n "tokio-util") (r "^0.7") (f (quote ("codec"))) (k 0)) (d (n "tracing") (r "^0.1") (f (quote ("attributes"))) (o #t) (k 0)) (d (n "tracing-subscriber") (r "^0.3") (k 2)))) (h "0np5k0h8i6dp0aqizx29vmsp8jhaz385i2akr44gx7k2hkkfvxi9") (f (quote (("default" "tracing"))))))
 
+(define-public crate-utf8-tokio-0.1.1 (c (n "utf8-tokio") (v "0.1.1") (d (list (d (n "tokio") (r "^1") (f (quote ("io-util"))) (k 0)) (d (n "tokio") (r "^1") (f (quote ("macros" "rt-multi-thread"))) (k 2)) (d (n "tokio-util") (r "^0.7.9") (f (quote ("codec"))) (k 0)) (d (n "tracing") (r "^0.1") (f (quote ("attributes"))) (o #t) (k 0)) (d (n "tracing-subscriber") (r "^0.3") (k 2)))) (h "04skp6qd5sr8njjfaw42bj2snnpjwzn2g399ghgsvyz40rs2wyd7") (f (quote (("default" "tracing"))))))
+
