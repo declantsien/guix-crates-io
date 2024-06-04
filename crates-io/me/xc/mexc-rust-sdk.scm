a31b34d9d1745d389e924eefdb1d0d1292bafcc0
GitHub Action <action@github.com>
2024-06-04 09:40:08 +0000

    crates-io: update

---
+++ crates-io/me/xc/mexc-rust-sdk.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io me xc mexc-rust-sdk) #:use-module (crates-io))
+
+(define-public crate-mexc-rust-sdk-0.5.0 (c (n "mexc-rust-sdk") (v "0.5.0") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "hmac") (r "^0.12.1") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.4") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)) (d (n "sha2") (r "^0.10.8") (d #t) (k 0)) (d (n "tokio") (r "^1.38.0") (f (quote ("macros" "rt-multi-thread" "full"))) (d #t) (k 0)) (d (n "url") (r "^2.5.0") (d #t) (k 0)))) (h "1nbkvn39fikpcd633jwmz3rvpawisqq03q5778wan8w8x0lxnpx7")))
+
