da58d0c43438450ed4b817b22bede970fdc37679
GitHub Action <action@github.com>
2024-06-04 21:51:53 +0000

    crates-io: update

---
+++ crates-io/co/mp/composefs.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io co mp composefs) #:use-module (crates-io))
+
+(define-public crate-composefs-0.1.0 (c (n "composefs") (v "0.1.0") (d (list (d (n "anyhow") (r "^1.0") (d #t) (k 0)) (d (n "composefs-sys") (r "^0.1.0") (d #t) (k 0)) (d (n "libc") (r "^0.2") (d #t) (k 0)) (d (n "tar") (r "^0.4.38") (d #t) (k 2)) (d (n "tempfile") (r "^3.2.0") (d #t) (k 2)))) (h "0dp5jjrhwmdvwarfmr7ny1bdlzf5sjxcm5ag4xgyjgk3ysh7q332") (f (quote (("v1_0_4" "composefs-sys/v1_0_4")))) (r "1.70.0")))
+
--- crates-io/gr/oc/grocer.scm
+++ crates-io/gr/oc/grocer.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-grocer-0.1.1 (c (n "grocer") (v "0.1.1") (d (list (d (n "image") (r "^0.25.1") (d #t) (k 0)))) (h "0i6mxb1whgn1jcw727gpsg9fagakcrgm0aagrr7rv95rkr5iipky")))
 
+(define-public crate-grocer-0.1.2 (c (n "grocer") (v "0.1.2") (d (list (d (n "image") (r "^0.25.1") (d #t) (k 0)))) (h "0vdcnq71d5riz59iv2p90kkpv4dscbvfyr2p61af8gwi3rhzj6xm")))
+
