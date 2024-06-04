6b894f26c0100f422aa8332117848e88a28e3ba4
GitHub Action <action@github.com>
2024-06-04 08:56:11 +0000

    crates-io: update

---
+++ crates-io/la/rk/lark-websocket-protobuf.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io la rk lark-websocket-protobuf) #:use-module (crates-io))
+
+(define-public crate-lark-websocket-protobuf-0.1.0 (c (n "lark-websocket-protobuf") (v "0.1.0") (d (list (d (n "bytes") (r "^1.6.0") (d #t) (k 0)) (d (n "prost") (r "^0.12.6") (d #t) (k 0)) (d (n "prost-build") (r "^0.12.6") (d #t) (k 1)))) (h "17fvamyab5vkcjckxws8hkb2s64yw46ypvb8kfhrpyk9khqp3dw8")))
+
