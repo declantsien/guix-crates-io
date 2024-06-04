0eb23e9fb1aa5a1ed9e8b3b0cb9ee73ea44bd777
GitHub Action <action@github.com>
2024-06-04 14:40:56 +0000

    crates-io: update

---
+++ crates-io/ra/xb/raxb-libxml2-sys.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ra xb raxb-libxml2-sys) #:use-module (crates-io))
+
+(define-public crate-raxb-libxml2-sys-0.3.2 (c (n "raxb-libxml2-sys") (v "0.3.2") (d (list (d (n "bindgen") (r "^0.69.4") (d #t) (k 1)) (d (n "cmake") (r "^0.1") (d #t) (k 1)))) (h "1gl96kxch3pjp158iid2p5hzgqqnshlri5qa3713s7mwkib6bddk") (r "1.78.0")))
+
