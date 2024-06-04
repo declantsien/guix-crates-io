933f5e1039cf8cf6a468e4eae3b82f83eb25cecb
GitHub Action <action@github.com>
2024-06-04 23:46:23 +0000

    crates-io: update

---
+++ crates-io/cp/p-/cpp-deps-cli.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io cp p- cpp-deps-cli) #:use-module (crates-io))
+
+(define-public crate-cpp-deps-cli-0.0.0 (c (n "cpp-deps-cli") (v "0.0.0") (h "1ii28j2nkq5xc3iyi6i4c1a72aks9xzymfpx0mbygkxjarkxyx8f")))
+
---
+++ crates-io/cp/p-/cpp-deps.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io cp p- cpp-deps) #:use-module (crates-io))
+
+(define-public crate-cpp-deps-0.0.0 (c (n "cpp-deps") (v "0.0.0") (d (list (d (n "cc") (r "^1.0") (f (quote ("parallel"))) (d #t) (k 0)) (d (n "dashmap") (r "^5.5") (k 0)) (d (n "memmap2") (r "^0.9.4") (d #t) (k 0)) (d (n "proptest") (r "^1.4.0") (d #t) (k 2)) (d (n "winnow") (r "^0.6") (d #t) (k 0)))) (h "1gvkail57s4p02vyvpsiwz1cs2llyr6brc2yz3da2mvg8vpmdza3")))
+
