cc299cf8291c5e2f92e3a280899f45800c2ca18f
GitHub Action <action@github.com>
2024-06-04 07:56:09 +0000

    crates-io: update

---
+++ crates-io/rl/_t/rl_tatam-rs.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io rl _t rl_tatam-rs) #:use-module (crates-io))
+
+(define-public crate-rl_tatam-rs-0.1.1 (c (n "rl_tatam-rs") (v "0.1.1") (d (list (d (n "clap") (r "^4.0.11") (f (quote ("derive"))) (d #t) (k 0)) (d (n "env_logger") (r "^0.8.4") (d #t) (k 0)) (d (n "heck") (r "^0.4.1") (d #t) (k 0)) (d (n "rl_model-rs") (r "^0.2.1") (d #t) (k 0)))) (h "1188cc16q0m1ndcj8ywj9pdcyrv16mnkl58w8qy3gfflsqn09yjc")))
+
---
+++ crates-io/tr/ee/treed_gpu.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io tr ee treed_gpu) #:use-module (crates-io))
+
+(define-public crate-treed_gpu-0.1.0 (c (n "treed_gpu") (v "0.1.0") (d (list (d (n "cc") (r "^1") (d #t) (k 1)) (d (n "clap") (r "^4") (f (quote ("derive"))) (d #t) (k 2)) (d (n "crossbeam-channel") (r "^0.5") (d #t) (k 0)) (d (n "digest") (r "^0.10") (d #t) (k 0)) (d (n "log") (r "^0.4") (d #t) (k 0)) (d (n "log4rs") (r "^1") (d #t) (k 2)) (d (n "sha2") (r "^0.10") (d #t) (k 0)))) (h "0ni191ixgrf1984bk6qsvr3chx4i3qxi9d2mblxiz8dams64daig")))
+
