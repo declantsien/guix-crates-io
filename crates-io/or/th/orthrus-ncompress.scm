5b92d49943948ab44d00fd0254a11b0061aaf010
GitHub Action <action@github.com>
2024-06-04 03:16:08 +0000

    crates-io: update

--- crates-io/or/th/orthrus-ncompress.scm
+++ crates-io/or/th/orthrus-ncompress.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-orthrus-ncompress-0.2.0 (c (n "orthrus-ncompress") (v "0.2.0") (d (list (d (n "orthrus-core") (r "^0.2.0") (d #t) (k 0)) (d (n "snafu") (r "^0.8") (k 0)))) (h "01zbbnbxr80h79xa2ssb225a0j6dbznpfkjyki6j1qs3xw99q2vp") (f (quote (("std") ("default" "std"))))))
 
+(define-public crate-orthrus-ncompress-0.2.1 (c (n "orthrus-ncompress") (v "0.2.1") (d (list (d (n "orthrus-core") (r "^0.2.0") (d #t) (k 0)) (d (n "snafu") (r "^0.8") (k 0)))) (h "1hfk0nx8b9akw4axir3zjq1q02iyb7yc0yd0fvi2x4m73jrhqr6j") (f (quote (("std") ("default" "std"))))))
+
