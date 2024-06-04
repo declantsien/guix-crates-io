2e41da476efb6405eedb21049333c6a0e5c5e3ac
GitHub Action <action@github.com>
2024-06-04 05:46:05 +0000

    crates-io: update

---
+++ crates-io/be/vy/bevy_scrolling_2d_camera.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io be vy bevy_scrolling_2d_camera) #:use-module (crates-io))
+
+(define-public crate-bevy_scrolling_2d_camera-0.1.0 (c (n "bevy_scrolling_2d_camera") (v "0.1.0") (d (list (d (n "bevy") (r "^0.13.0") (d #t) (k 0)))) (h "0bfhigmavhvq0q239r2i4mpda8f9hw34qy1vndpiy5324n4b95sy")))
+
--- crates-io/wb/_s/wb_sqlite.scm
+++ crates-io/wb/_s/wb_sqlite.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-wb_sqlite-0.1.1 (c (n "wb_sqlite") (v "0.1.1") (d (list (d (n "convert_case") (r "^0.6.0") (d #t) (k 0)) (d (n "rusqlite") (r "^0.30.0") (f (quote ("bundled"))) (d #t) (k 2)) (d (n "sqlx") (r "^0.7.4") (f (quote ("sqlite" "runtime-tokio"))) (d #t) (k 2)) (d (n "tokio") (r "^1.38.0") (f (quote ("rt" "macros"))) (d #t) (k 2)) (d (n "virtue") (r "^0.0.16") (d #t) (k 0)))) (h "15018csds04ys0ansq23n503bnq5hihay633zijss4gk2br0akyz")))
 
+(define-public crate-wb_sqlite-0.1.2 (c (n "wb_sqlite") (v "0.1.2") (d (list (d (n "convert_case") (r "^0.6.0") (d #t) (k 0)) (d (n "rusqlite") (r "^0.30.0") (f (quote ("bundled"))) (d #t) (k 2)) (d (n "sqlx") (r "^0.7.4") (f (quote ("sqlite" "runtime-tokio"))) (d #t) (k 2)) (d (n "tokio") (r "^1.38.0") (f (quote ("rt" "macros"))) (d #t) (k 2)) (d (n "virtue") (r "^0.0.16") (d #t) (k 0)))) (h "11n54zw4v4bwwvmc8w1ayf2qfhhyvqfar6c7yq1ykdy11cvzb300")))
+
