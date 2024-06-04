a2f7e340ce956e4deb9b87ea90a82e50174fa038
GitHub Action <action@github.com>
2024-06-04 19:23:32 +0000

    crates-io: update

--- crates-io/ma/xm/maxmatrix_rs.scm
+++ crates-io/ma/xm/maxmatrix_rs.scm
@@ -1,8 +1,8 @@
 (define-module (crates-io ma xm maxmatrix_rs) #:use-module (crates-io))
 
-(define-public crate-maxmatrix_rs-0.1.0 (c (n "maxmatrix_rs") (v "0.1.0") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)))) (h "15d8arb2yqs07kbiyam9pfgawvsqssqx9y1y3y8l64ky917y623g")))
+(define-public crate-maxmatrix_rs-0.1.0 (c (n "maxmatrix_rs") (v "0.1.0") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)))) (h "15d8arb2yqs07kbiyam9pfgawvsqssqx9y1y3y8l64ky917y623g") (y #t)))
 
-(define-public crate-maxmatrix_rs-0.1.1 (c (n "maxmatrix_rs") (v "0.1.1") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)))) (h "1g3a285h4cg6yx7p7zzyfzvrp62pcj0ackrj6dj88icx9aasggqc")))
+(define-public crate-maxmatrix_rs-0.1.1 (c (n "maxmatrix_rs") (v "0.1.1") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)))) (h "1g3a285h4cg6yx7p7zzyfzvrp62pcj0ackrj6dj88icx9aasggqc") (y #t)))
 
 (define-public crate-maxmatrix_rs-0.1.2 (c (n "maxmatrix_rs") (v "0.1.2") (d (list (d (n "embedded-hal") (r "^0.2.3") (d #t) (k 0)))) (h "1hnhsh9m5fglkhy884xcmhzv38ngpbjbwchfw7mwvnvpkfm5q5sv")))
 
--- crates-io/ra/tr/ratrod-rs.scm
+++ crates-io/ra/tr/ratrod-rs.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-RatRod-rs-0.2.0 (c (n "RatRod-rs") (v "0.2.0") (d (list (d (n "dylib") (r "^0.0.3") (d #t) (k 0)) (d (n "more-asserts") (r "^0.3.1") (d #t) (k 0)) (d (n "serde") (r "^1.0.196") (f (quote ("derive" "rc"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.113") (d #t) (k 0)) (d (n "serde_json_any_key") (r "^2.0.0") (d #t) (k 0)) (d (n "sparse_matrix") (r "^0.6.2") (d #t) (k 0)))) (h "1qx2i23g21g64vgfncfbp5jgxhv0cwg6mv4bal6w5q5aj68x4g7i")))
 
+(define-public crate-RatRod-rs-0.2.1 (c (n "RatRod-rs") (v "0.2.1") (d (list (d (n "dylib") (r "^0.0.3") (d #t) (k 0)) (d (n "more-asserts") (r "^0.3.1") (d #t) (k 0)) (d (n "serde") (r "^1.0.196") (f (quote ("derive" "rc"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.113") (d #t) (k 0)) (d (n "serde_json_any_key") (r "^2.0.0") (d #t) (k 0)) (d (n "sparse_matrix") (r "^0.6.3") (d #t) (k 0)))) (h "0zfk46x2366dab42lnp8pyx3hsrd6na8i1j5z6j4czq4bjkb006a")))
+
