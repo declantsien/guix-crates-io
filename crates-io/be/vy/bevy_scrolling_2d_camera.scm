d38caef23906546c2a74c1c3773c1d0cc06b576b
GitHub Action <action@github.com>
2024-06-04 07:37:23 +0000

    crates-io: update

--- crates-io/be/vy/bevy_scrolling_2d_camera.scm
+++ crates-io/be/vy/bevy_scrolling_2d_camera.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-bevy_scrolling_2d_camera-0.1.0 (c (n "bevy_scrolling_2d_camera") (v "0.1.0") (d (list (d (n "bevy") (r "^0.13.0") (d #t) (k 0)))) (h "0bfhigmavhvq0q239r2i4mpda8f9hw34qy1vndpiy5324n4b95sy")))
 
+(define-public crate-bevy_scrolling_2d_camera-0.1.1 (c (n "bevy_scrolling_2d_camera") (v "0.1.1") (d (list (d (n "bevy") (r "^0.13.0") (d #t) (k 0)))) (h "0qj9rks1rcnycb29a769qx62l4zd2yqrnnqvirvbazff4aq9m9wa")))
+
---
+++ crates-io/ri/de/ride.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ri de ride) #:use-module (crates-io))
+
+(define-public crate-ride-0.0.0 (c (n "ride") (v "0.0.0") (h "1lfg1hprd0fsd1sp270b8zg9gbr9i5srlvngvp1cy5n26zdlkhmf")))
+
