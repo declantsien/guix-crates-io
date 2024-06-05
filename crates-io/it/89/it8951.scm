b09ac1c33850f14bb5f817612c967d07fd9ad3f1
GitHub Action <action@github.com>
2024-06-05 11:23:40 +0000

    crates-io: update

--- crates-io/it/89/it8951.scm
+++ crates-io/it/89/it8951.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-it8951-0.2.0 (c (n "it8951") (v "0.2.0") (d (list (d (n "embedded-graphics") (r "^0.8.1") (d #t) (k 2)) (d (n "embedded-graphics-core") (r "^0.4.0") (d #t) (k 0)) (d (n "embedded-hal") (r "^1") (d #t) (k 0)) (d (n "linux-embedded-hal") (r "^0.4") (d #t) (k 2)))) (h "1px01dl6xglpxnx1ayh7byma0b4xwadp0wqyvm2xmkca5xqfmv41")))
 
+(define-public crate-it8951-0.3.0 (c (n "it8951") (v "0.3.0") (d (list (d (n "embedded-graphics") (r "^0.8.1") (d #t) (k 2)) (d (n "embedded-graphics-core") (r "^0.4.0") (d #t) (k 0)) (d (n "embedded-hal") (r "^1") (d #t) (k 0)) (d (n "linux-embedded-hal") (r "^0.4") (d #t) (k 2)))) (h "0j44f3bjqnvgy0lcbpmgpn64nlkg647ah37c33b1myzrr7smahhs")))
+
