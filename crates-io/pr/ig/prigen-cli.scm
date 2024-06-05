93d70bd63579863a01415d51e5197f8785c3aa15
GitHub Action <action@github.com>
2024-06-05 21:46:38 +0000

    crates-io: update

---
+++ crates-io/pr/ig/prigen-cli.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io pr ig prigen-cli) #:use-module (crates-io))
+
+(define-public crate-prigen-cli-0.1.0 (c (n "prigen-cli") (v "0.1.0") (d (list (d (n "bytes") (r "^1.6.0") (d #t) (k 0)) (d (n "clap") (r "^4.5.4") (f (quote ("derive" "cargo"))) (d #t) (k 0)) (d (n "inquire") (r "^0.7.5") (d #t) (k 0)) (d (n "regex") (r "^1.10.4") (d #t) (k 0)) (d (n "reqwest") (r "^0.12.4") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "tokio") (r "^1.38.0") (f (quote ("full"))) (d #t) (k 0)))) (h "1bxanpgnxpdn9z8lw8qb6iff4qddp194xrasry432qbldj0qlkci")))
+
