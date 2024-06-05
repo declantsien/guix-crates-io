87cc006dab65a5a230f5abed53c01c10b1a6cbaa
GitHub Action <action@github.com>
2024-06-05 02:47:56 +0000

    crates-io: update

---
+++ crates-io/re/tr/retrom-db.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io re tr retrom-db) #:use-module (crates-io))
+
+(define-public crate-retrom-db-0.0.1 (c (n "retrom-db") (v "0.0.1") (d (list (d (n "async-trait") (r "^0.1") (d #t) (k 0)) (d (n "bb8") (r "^0.8") (d #t) (k 0)) (d (n "derive_builder") (r "^0.20.0") (d #t) (k 0)) (d (n "diesel") (r "^2.1.5") (f (quote ("serde_json" "numeric"))) (k 0)) (d (n "diesel-async") (r "^0.4.1") (f (quote ("postgres" "bb8"))) (d #t) (k 0)) (d (n "dotenvy") (r "^0.15.7") (d #t) (k 0)) (d (n "serde") (r "^1.0.197") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.115") (d #t) (k 0)) (d (n "tracing") (r "^0.1.40") (d #t) (k 0)))) (h "19pg9ysvx92yyq4i8jvzzwja19qk05cp1ngj245hhfawaln36h80")))
+
