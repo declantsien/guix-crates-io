42f189b8ba7947c3e4393b29f2ba2e8e30911223
GitHub Action <action@github.com>
2024-06-05 13:39:31 +0000

    crates-io: update

--- crates-io/js/eq/jseqio.scm
+++ crates-io/js/eq/jseqio.scm
@@ -6,3 +6,5 @@
 
 (define-public crate-jseqio-0.1.2 (c (n "jseqio") (v "0.1.2") (d (list (d (n "ex") (r "^0.1.3") (d #t) (k 0)) (d (n "flate2") (r "^1.0.24") (d #t) (k 0)))) (h "107447pz6ph9356p5lr5g6bf3210wwhrzcd0r4pmsidag1p7g1gb")))
 
+(define-public crate-jseqio-0.1.3 (c (n "jseqio") (v "0.1.3") (d (list (d (n "ex") (r "^0.1.3") (d #t) (k 0)) (d (n "flate2") (r "^1.0.24") (d #t) (k 0)))) (h "10q8yl4mpsjs4m9kv0h8lmq8jqafkkvn24l71y049sficknhv575")))
+
