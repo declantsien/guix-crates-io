b4bd53fdd0b144c003860b0fd6ca12f84de8c7a9
GitHub Action <action@github.com>
2024-06-05 04:56:19 +0000

    crates-io: update

---
+++ crates-io/yt/tr/yttria-coding.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io yt tr yttria-coding) #:use-module (crates-io))
+
+(define-public crate-yttria-coding-0.1.0 (c (n "yttria-coding") (v "0.1.0") (h "143k68nc9s0bj8r2mswcvpcd1flbi6c7dlaa65cihw3l4jdg2j4j")))
+
