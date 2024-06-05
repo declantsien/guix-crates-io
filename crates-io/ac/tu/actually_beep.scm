7098efc806cfc5b6e913080668014fed7fa54d31
GitHub Action <action@github.com>
2024-06-05 03:46:03 +0000

    crates-io: update

--- crates-io/ac/tu/actually_beep.scm
+++ crates-io/ac/tu/actually_beep.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-actually_beep-0.1.0 (c (n "actually_beep") (v "0.1.0") (d (list (d (n "cpal") (r "^0.15.3") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.61") (d #t) (k 0)))) (h "029yjl9zr6wh124gccyd45mzxqncgm2a84wvljq8zc42z1lrs73w") (f (quote (("threaded") ("jack" "cpal/jack")))) (r "1.70.0")))
 
+(define-public crate-actually_beep-0.1.1 (c (n "actually_beep") (v "0.1.1") (d (list (d (n "cpal") (r "^0.15.3") (d #t) (k 0)) (d (n "thiserror") (r "^1.0.61") (d #t) (k 0)))) (h "18wgn6g25smxh2kky36qw0zkk7yw4sjmha76g36wry403hx7hm1f") (f (quote (("threaded") ("jack" "cpal/jack")))) (r "1.70.0")))
+
--- crates-io/de/eb/deeb.scm
+++ crates-io/de/eb/deeb.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-deeb-0.0.1 (c (n "deeb") (v "0.0.1") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "env_logger") (r "^0.11.3") (d #t) (k 0)) (d (n "fs2") (r "^0.4.3") (d #t) (k 0)) (d (n "log") (r "^0.4.21") (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "05y32sra7z5vrhjgpwb69ncknfsii7636vv332lcjwh4426xiqx9")))
 
+(define-public crate-deeb-0.0.2 (c (n "deeb") (v "0.0.2") (d (list (d (n "anyhow") (r "^1.0.86") (d #t) (k 0)) (d (n "criterion") (r "^0.4") (f (quote ("html_reports" "async_tokio"))) (d #t) (k 2)) (d (n "env_logger") (r "^0.11.3") (d #t) (k 0)) (d (n "fs2") (r "^0.4.3") (d #t) (k 0)) (d (n "log") (r "^0.4.21") (d #t) (k 0)) (d (n "serde") (r "^1.0.203") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1.0.117") (d #t) (k 0)) (d (n "tokio") (r "^1.37.0") (f (quote ("full"))) (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (f (quote ("v4"))) (d #t) (k 0)))) (h "16mhzij05hsnc1dfsvsclxxg369bj11ha1a52a2c2ws03fzxjm35")))
+
