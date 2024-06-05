9aa10507d243aaf84004936fcd8112188f3c7e0e
GitHub Action <action@github.com>
2024-06-05 17:37:36 +0000

    crates-io: update

---
+++ crates-io/ha/st/haste.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ha st haste) #:use-module (crates-io))
+
+(define-public crate-haste-0.0.0 (c (n "haste") (v "0.0.0") (h "0bavfpb2539q9l7phids45498hi0sgb6knyasidrn79yb9agsqcd")))
+
--- crates-io/py/bi/pybites-search.scm
+++ crates-io/py/bi/pybites-search.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-pybites-search-0.1.0 (c (n "pybites-search") (v "0.1.0") (d (list (d (n "cached") (r "^0.34.0") (d #t) (k 0)) (d (n "regex") (r "^1") (d #t) (k 0)) (d (n "reqwest") (r "^0.11") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "13nxq237rc6ypwvhgx1yzjn6m63l6l0n12lcjm583pykgpvg0rn2")))
 
+(define-public crate-pybites-search-0.2.0 (c (n "pybites-search") (v "0.2.0") (d (list (d (n "cached") (r "^0.34.0") (d #t) (k 0)) (d (n "dirs") (r "^4.0") (d #t) (k 0)) (d (n "regex") (r "^1") (d #t) (k 0)) (d (n "reqwest") (r "^0.11") (f (quote ("json"))) (d #t) (k 0)) (d (n "serde") (r "^1") (f (quote ("derive"))) (d #t) (k 0)) (d (n "serde_json") (r "^1") (d #t) (k 0)) (d (n "tokio") (r "^1") (f (quote ("full"))) (d #t) (k 0)))) (h "0ynhz81dshc56pf334lysijs7991lywyyw4hpg9n3s3wqd3clfpv")))
+
