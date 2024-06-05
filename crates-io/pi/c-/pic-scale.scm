86f101cea8f0ea123b237ac837b0b3497861f19b
GitHub Action <action@github.com>
2024-06-05 15:56:13 +0000

    crates-io: update

---
+++ crates-io/pi/c-/pic-scale.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io pi c- pic-scale) #:use-module (crates-io))
+
+(define-public crate-pic-scale-0.1.0 (c (n "pic-scale") (v "0.1.0") (d (list (d (n "colorutils-rs") (r "^0.2.9") (d #t) (k 0)) (d (n "num-traits") (r "^0.2.19") (d #t) (k 0)) (d (n "rayon") (r "^1.10.0") (d #t) (k 0)))) (h "15vd09v9698g0vhs57pwlzrqc3izf34v8jaxlyrwiklsapiwvk3j")))
+
