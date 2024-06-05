d6002e5e6bf69bbdf4b26a10b9b43d6e2ec7ef58
GitHub Action <action@github.com>
2024-06-05 23:57:03 +0000

    crates-io: update

--- crates-io/aw/s-/aws-ip-ranges.scm
+++ crates-io/aw/s-/aws-ip-ranges.scm
@@ -662,3 +662,5 @@
 
 (define-public crate-aws-ip-ranges-0.332.0 (c (n "aws-ip-ranges") (v "0.332.0") (d (list (d (n "serde") (r "^1.0.192") (f (quote ("derive"))) (d #t) (k 1)) (d (n "serde_json") (r "^1.0.108") (d #t) (k 1)))) (h "13z1bi54jywbiqbixl81lldkgvj9df0c0jv807n0nkxhavl0xi66")))
 
+(define-public crate-aws-ip-ranges-0.333.0 (c (n "aws-ip-ranges") (v "0.333.0") (d (list (d (n "serde") (r "^1.0.192") (f (quote ("derive"))) (d #t) (k 1)) (d (n "serde_json") (r "^1.0.108") (d #t) (k 1)))) (h "088x2sc9xidh8ildwdghvgwbwsny002hq9z65k8wdy6i2l45j71q")))
+
