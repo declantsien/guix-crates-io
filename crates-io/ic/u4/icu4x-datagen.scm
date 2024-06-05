43e35841aeab8c53127c2d013f84d9bffe2edd14
GitHub Action <action@github.com>
2024-06-05 21:29:19 +0000

    crates-io: update

---
+++ crates-io/ic/u4/icu4x-datagen.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io ic u4 icu4x-datagen) #:use-module (crates-io))
+
+(define-public crate-icu4x-datagen-0.0.1 (c (n "icu4x-datagen") (v "0.0.1") (h "167fm9c090qy1czr3509ibh79zgga4ncn4z7fb2s7kyj7y9jdvk9")))
+
--- crates-io/ma/ti/matic-portal-types.scm
+++ crates-io/ma/ti/matic-portal-types.scm
@@ -2,3 +2,5 @@
 
 (define-public crate-matic-portal-types-0.1.0 (c (n "matic-portal-types") (v "0.1.0") (d (list (d (n "serde") (r "^1.0.195") (f (quote ("derive"))) (d #t) (k 0)) (d (n "thiserror") (r "^1.0.57") (d #t) (k 0)))) (h "13kq2jzp2j9rndkqgj586jyg2702q7a9ss19kf37s2i44spf5sk6") (r "1.70.0")))
 
+(define-public crate-matic-portal-types-0.2.0 (c (n "matic-portal-types") (v "0.2.0") (d (list (d (n "serde") (r "^1.0.187") (f (quote ("derive"))) (d #t) (k 0)) (d (n "thiserror") (r "^1.0.40") (d #t) (k 0)))) (h "15yjmxm4axj44l6p44sj1g1bdxirrm3ns9ly41gaizn4iw1qcv84") (r "1.70.0")))
+
