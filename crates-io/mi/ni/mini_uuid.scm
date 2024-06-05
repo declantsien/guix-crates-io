9a0361c926c1b2d19ef736cfd4f194aa52251376
GitHub Action <action@github.com>
2024-06-05 19:22:25 +0000

    crates-io: update

--- crates-io/mi/ni/mini_uuid.scm
+++ crates-io/mi/ni/mini_uuid.scm
@@ -4,3 +4,5 @@
 
 (define-public crate-mini_uuid-0.1.1 (c (n "mini_uuid") (v "0.1.1") (d (list (d (n "base64") (r "^0.22.1") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (d #t) (k 0)))) (h "10di2fn8mj6g718qs6b1k1w5q6whzfbjgy5fk2wzjbyppy0g5hx1") (f (quote (("v7" "uuid/v7") ("v4" "uuid/v4") ("v1" "uuid/v1") ("url_safe") ("padded") ("default" "v1" "url_safe" "padded"))))))
 
+(define-public crate-mini_uuid-0.2.0 (c (n "mini_uuid") (v "0.2.0") (d (list (d (n "base64") (r "^0.22.1") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (d #t) (k 0)))) (h "11kybv1bwhr0579mvw8kfba71r5nvxfd2xy27x5vzv0agl29llyw") (f (quote (("v7" "uuid/v7") ("v4" "uuid/v4") ("v1" "uuid/v1") ("url_safe") ("padded") ("default" "v4" "url_safe" "padded"))))))
+
