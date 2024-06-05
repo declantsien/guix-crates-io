84004ea70412ec6690a4079155c565109997d32c
GitHub Action <action@github.com>
2024-06-05 22:48:58 +0000

    crates-io: update

---
+++ crates-io/tu/rd/turdle.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io tu rd turdle) #:use-module (crates-io))
+
+(define-public crate-turdle-0.1.0 (c (n "turdle") (v "0.1.0") (d (list (d (n "anyhow") (r "^1.0.82") (d #t) (k 0)) (d (n "crossterm") (r "^0.27.0") (d #t) (k 0)) (d (n "indexmap") (r "^2.2.6") (d #t) (k 0)) (d (n "rand") (r "^0.8.5") (d #t) (k 0)) (d (n "tui-big-text") (r "^0.4.3") (d #t) (k 0)) (d (n "tui-realm-stdlib") (r "^1.3.2") (d #t) (k 0)) (d (n "tuirealm") (r "^1.9") (f (quote ("ratatui" "crossterm"))) (k 0)))) (h "08xxr9vhd39bxmh246yhkr6mbg05hvqkradb8nq8q56yx0mmm4fn")))
+
