877e2a88dc9ca5d3892b0e2dadc98499d96a8f0e
GitHub Action <action@github.com>
2024-06-05 03:56:02 +0000

    crates-io: update

---
+++ crates-io/mi/ni/mini_uuid.scm
@@ -0,0 +1,4 @@
+(define-module (crates-io mi ni mini_uuid) #:use-module (crates-io))
+
+(define-public crate-mini_uuid-0.1.0 (c (n "mini_uuid") (v "0.1.0") (d (list (d (n "base64") (r "^0.22.1") (d #t) (k 0)) (d (n "hex") (r "^0.4.3") (d #t) (k 0)) (d (n "uuid") (r "^1.8.0") (d #t) (k 0)))) (h "1h1cl0hdlw37zmn02mc82qh9drz41jqrhgmiyy2bg5rpgr6ws0rv") (f (quote (("v7" "uuid/v7") ("v4" "uuid/v4") ("v1" "uuid/v1") ("url_safe") ("padded") ("default" "v4" "url_safe" "padded"))))))
+
